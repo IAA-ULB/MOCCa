95580333b1f9f2d6289a13b7bce7e6c4f7544097
Wouter <w.ryssens@ipnl.in2p3.fr>
2018-08-09 19:45:28 +0200

    Bugfixed extra SolvePairing.f90

--- src_orig/tantalus.f90
+++ src_orig/tantalus.f90
@@ -163,13 +163,14 @@ subroutine ReachForWaterAndFood
           
           ! Do an approximate projection on the feasible set
           call feasibleproject()
+
+          ! Solve the pairing problem.
+          call SolvePairing()
         endif
-        
+ 
         ! Restore all the different derivatives.
         call deriveall()
-        ! Solve the pairing problem.
-        call SolvePairing()
-        
+ 
         ! Update the densities
         if(projectpresent) then 
           call densit(SaveRho=.false.)
