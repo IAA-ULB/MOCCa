001cda8b45d516ddc3b10256a5b77c70bd31c0d8
Wouter@Orome <wouter.ryssens@gmail.com>
2021-07-30 18:42:09 +0200

    Added (i)  DD-switch-TP config file        (ii) Adapted gen_public_version.sh to include the _TP version of the code

---
+++ configs/DD-switch-TP.py
@@ -0,0 +1,12 @@
+# Configuration file for Tantalus compilation
+# Functional : NLO Skyrme-type (think SLy4/5/6) with optional Jmn terms
+FUNC_FILE = 'DD-switch.func'
+# Symmetries : EV4-style
+SYMSTRING = 'Rz,STy'
+REDUCE    = [1,1,0]
+# Read symmetries: EV8-style
+INSYM     = 'Rz,T,STy'
+INREDUCE  = [1,1,0]
+# Axis orientation for multipole moments : default
+QUANT_AXIS='Z'
+SECOND_AXIS=1
--- gen_public_version.sh
+++ gen_public_version.sh
@@ -2,10 +2,12 @@
 CONFIG=DD-switch
 CONFIG_T=DD-switch-T
 CONFIG_P=DD-switch-P
+CONFIG_TP=DD-switch-TP
 
 SRCPUBLIC=$HOME/Documents/Codes/tantalus_public/src
 SRCPUBLIC_T=$HOME/Documents/Codes/tantalus_public/src_T
 SRCPUBLIC_P=$HOME/Documents/Codes/tantalus_public/src_P
+SRCPUBLIC_TP=$HOME/Documents/Codes/tantalus_public/src_TP
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
 # Time-reversal conserved version
@@ -44,4 +46,17 @@ sed -i.bak 's/VERSION3/"${GIT_INFO3}"/' src/tantalus.version.f90
 rm src/tantalus.version.f90.bak
 
 cp src/*.f90 $SRCPUBLIC_P/
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+# PT-broken version
+
+python3 Hephaestos.py $CONFIG_TP
+
+cp src/tantalus.f90 src/tantalus.version.f90
+sed -i.bak 's/VERSION1/"${GIT_INFO1}"/' src/tantalus.version.f90 
+sed -i.bak 's/VERSION2/"${GIT_INFO2}"/' src/tantalus.version.f90 
+sed -i.bak 's/VERSION3/"${GIT_INFO3}"/' src/tantalus.version.f90 
+rm src/tantalus.version.f90.bak
+
+cp src/*.f90 $SRCPUBLIC_TP/
 
