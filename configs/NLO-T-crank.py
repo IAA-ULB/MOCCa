034e2db64a917d922392f39def3346dc34789157
Wouter@Orome <wouter.ryssens@gmail.com>
2021-10-21 11:49:53 +0200

    Added DD-switch-T-crank and NLO-T-crank, and actually started tracking the old parameterizations

---
+++ configs/NLO-T-crank.py
@@ -0,0 +1,14 @@
+# Configuration file for Tantalus compilation
+# Functional : NLO Skyrme-type (think SLy4/5/6) with optional Jmn terms
+# Multipole moments quantised along the X direction
+FUNC_FILE = 'NLO.func'
+# Symmetries : EV8-style
+SYMSTRING = 'Rz,P,STy'
+REDUCE    = [1,1,1]
+# Read symmetries: EV8-style
+INSYM     = 'Rz,T,P,STy'
+INREDUCE  = [1,1,1]
+# Axis orientation for multipole moments : X
+QUANT_AXIS='X'
+SECOND_AXIS=2
+
