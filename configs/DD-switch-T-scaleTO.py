9fa0fa3e7c45063399c2300f9f3c77412655044b
Wouter@Orome <wouter.ryssens@gmail.com>
2021-11-24 11:06:05 +0100

    Added (.func, configuration, parameterization) files to explore the impact of time-odd terms

---
+++ configs/DD-switch-T-scaleTO.py
@@ -0,0 +1,10 @@
+# Configuration file for Tantalus compilation
+FUNC_FILE = 'DD-switch-scaleTO.func'
+SYMSTRING = 'Rz,P,STy'
+REDUCE    = [1,1,1]
+# Read symmetries: EV8-style
+INSYM     = 'Rz,T,P,STy'
+INREDUCE  = [1,1,1]
+# Axis orientation for multipole moments : default
+QUANT_AXIS='Z'
+SECOND_AXIS=1
