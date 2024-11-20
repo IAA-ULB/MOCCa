fcb97227ff5ed7ccac9bbe766cea7fc61bc05621
Wouter@Orome <wouter.ryssens@gmail.com>
2024-11-20 15:50:52 +0100

    Clean-up of unused CONFIG and FUNCTIONAL files
      No longer useful
      ----------------
      deleted:    configs/NLO-PRz.py
      deleted:    functionals/BXL_old.func
    
      Partially fixed
      ----------------
      modified:   functionals/BXL-N2LO-full.func
    
      Things that do not currently work but might be useful in the future
      ----------------------------
      renamed:    functionals/N2LO.full.func -> functionals/work_in_progress/N2LO.full.func
      renamed:    functionals/N2LO.paul.func -> functionals/work_in_progress/N2LO.paul.func
      new file:   functionals/work_in_progress/NLO+Fayans.func
      renamed:    configs/N2LO-full-T.py -> configs/work_in_progress/N2LO-full-T.py
      renamed:    configs/BXL-T-scaleTO.py -> configs/work_in_progress/NLO+Fayans.py
      renamed:    functionals/N2LO.Becker.func -> functionals/work_in_progress/N2LO.Becker.func

--- configs/work_in_progress/NLO+Fayans.py
+++ configs/work_in_progress/NLO+Fayans.py
@@ -1,6 +1,8 @@
 # Configuration file for Tantalus compilation
-FUNC_FILE = 'BXL-scaleTO.func'
-SYMSTRING = 'Rz,P,STy'
+# Functional : NLO Skyrme p-h channel + Fayans pairing
+FUNC_FILE = 'NLO+Fayans.func'
+# Symmetries : EV8-style
+SYMSTRING = 'Rz,T,P,STy'
 REDUCE    = [1,1,1]
 # Read symmetries: EV8-style
 INSYM     = 'Rz,T,P,STy'
@@ -9,4 +11,4 @@ INREDUCE  = [1,1,1]
 QUANT_AXIS='Z'
 SECOND_AXIS=1
 # Decouple the particle-hole and particle-particle fields? 
-PH_PP_DECOUPL = True
+PH_PP_DECOUPL = False
