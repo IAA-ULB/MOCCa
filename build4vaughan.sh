f4384bd6193c4c613eff278b68c2aaeda7fc09a5
the-john-doe <john.doe@example.com>
2025-06-10 16:18:36 +0200

    Add a script for building PASTA mode on Vaughan

---
+++ build4vaughan.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# First try to build the pasta code
+
+ml calcua/2024a
+ml ScaLAPACK
+ml Python
+ml SciPy-bundle
+ml make
+
+ml 
+
+mkdir -p src
+mkdir -p mod
+mkdir -p obj
+
+make INCLUDE=gnu-parallel CALCTYPE=PASTA
