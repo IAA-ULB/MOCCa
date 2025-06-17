365a00520bc4cdfd842e4d87e4aaf3525980b992
Wouter Ryssens <wouter.ryssens@ulb.be>
2025-06-17 10:16:46 +0200

    Documentation of the new FAM routines

--- src_orig/functional.f90
+++ src_orig/functional.f90
@@ -1419,11 +1419,20 @@ $CALCPOTENTIALS
   
   subroutine calc_perturbed_potentials(R,dRs,dRa, dFs, dFa)
     !---------------------------------------------------------------------------
-    ! TODO: improve documentation
+    ! Calculate the linearised response of the potentials (dFs, dFa) around
+    ! a set of mean-field densities (R) that are affected by perturbations
+    ! (dRs, dRa).
     !
+    ! There is a clear separation between so-called symmetric and antisymmetric
+    ! quantities, both for the densities and potentials. The TOTAL perturbation
+    ! is in both cases the sum of the symmetric and antisymmetric parts, but
+    ! these parts have different symmetry properties such that we cannot simply
+    ! sum them everywhere.
     !
-    ! Note: the potentials coming out of this function do not behave nicely 
-    !       w.r.t. symmetries!
+    ! For the potentials, the expressions for the symmetric and antisymmetric
+    ! part are identical; this routine is thus not much more than a wrapper
+    ! around two function calls to calc_perturbed_potentials.
+    !
     ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     ! Input:
     !   R   : density-vector containing the mean-field densities
@@ -1447,8 +1456,8 @@ $CALCPOTENTIALS
   
   function calc_perturbed_potentials_oneoff(R,R_pert) result (F)
     !---------------------------------------------------------------------------
-    ! TODO: improve documentation
-    !
+    ! Calculate the perturbed potential vector (F) around a set of mean-field
+    ! densities (R) affected by a perturbation (R_pert).
     ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     ! Input:
     !   R    : density-vector containing the mean-field densities
@@ -1456,8 +1465,6 @@ $CALCPOTENTIALS
     ! Output:
     !   F: potential-vector containing the linearised response of the mean-field
     !      potentials/
-    !
-    ! TODO: add Coulomb
     !---------------------------------------------------------------------------
     use Coulombmod       , only : SolveCoulomb
     
