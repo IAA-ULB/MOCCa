2d04cdf8a70cd0fcae74da173727cae5328db929
the-john-doe <john.doe@example.com>
2025-07-11 10:37:32 +0200

    Clean up

--- experiments/elpa/test_real_example/cl.sh
+++ experiments/elpa/test_real_example/cl.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# # Following the manual "4 Compiling and linking against ELPA" p28 
-# # FCFLAGS, CFLAGS, CXXFLAGS, and LDFLAGS.
-# # 1. Extend the PKG_CONFIG_PATH environment variable to point to the subfolder lib/pkgconfig
-# export PKG_CONFIG_PATH=/data/antwerpen/201/vsc20170/buildingElpa/elpa/_install/lib/pkgconfig
-# # 2. Fetch the correct flags for Fortran (FCFLAGS)
-# export FCFLAGS=$(pkg-config --variable=fcflags elpa)
-# # 4. Fetch the correct linker flags (LDFLAGS)
-# export LDFLAGS=$(pkg-config --libs elpa)
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/antwerpen/201/vsc20170/buildingElpa/elpa/_install-intel/lib
-
-# echo '${FCFLAGS}=' ${FCFLAGS}
-# echo "LDFLAGS="${LDFLAGS}
-
 make && mpirun -np 4 ./exe 8 2
