6dc4efb5ec4f390ba0f0134691a344ec3e6d20c2
the-john-doe <john.doe@example.com>
2025-07-20 01:53:50 +0200

    Add plotting functionality

--- experiments/elpa/pyev_dev/timings/pp.py
+++ experiments/elpa/pyev_dev/timings/pp.py
@@ -2,7 +2,10 @@
 
 import sys
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 import re
+
 
 def collect_timings():
     print("collecting:")
@@ -41,12 +44,41 @@ def select(timings, kwargs):
             selection[filename] = cputime
             print(filename,cputime)
 
-    return selection
+    return selection, criteria
 
+def plot(selection, criteria=None, x=None, title=None):
+    if x is None:
+        raise ValueError("ERROR: you must provide x.")
+    if not criteria and not title:
+        raise ValueError("ERROR: criteria and title must not both be None.")
+    
+    _title = f"{criteria}" if not title else title
+    
+    pattern = re.compile(f"{x}=(\d+)")
+    xd = []
+    yd = []
+    for filename,cputime in selection.items():
+        m = pattern.findall(filename)
+        xd.append(int(m[0]))
+        yd.append(cputime)
+    xd = np.array(xd)
+    yd = np.array(yd)
+    idx = np.argsort(xd)
+    xd = xd[idx]
+    yd = yd[idx]
 
+    plt.figure()
+    plt.title(title)
+    plt.xlabel(x)
+    plt.ylabel(f"walltime [s]")
+    plt.plot(xd,yd,'o--')
+    plt.savefig(f"{_title}.png")
+    plt.close()
+    
 if __name__ == '__main__':
     if '--collect' in sys.argv:
         timings = collect_timings()
-    selection = select(timings,{'nnodes':1, 'x':(4,4)})
-        
-        
+    selection, criteria = select(timings,{'nnodes':1, 'x':(4,4)})
+    # plot(timings, x='nnodes')        
+    plot(timings, x='nranks', title='walltime=f(nranks)')    
+
