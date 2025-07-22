d3ddfa817bc504a53cd0476547b40b093f7cda51
the-john-doe <john.doe@example.com>
2025-07-22 13:06:40 +0200

    Modify run-tests.sh

--- experiments/elpa/pyev_dev/run-tests.sh
+++ experiments/elpa/pyev_dev/run-tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# use pytest -x for stopping at the first failure
+
+export USE_PYELPA=1
+echo "USE_PYELPA=${USE_PYELPA}"
+mpirun -n 4 python -m pytest -s tests
+
+# this one fails often because not all Elpa functionality is implemented in pyev
 export USE_PYELPA=0
 echo "USE_PYELPA=${USE_PYELPA}"
-mpirun -n 4 python -m pytest -s -x tests
-
-# export USE_PYELPA=1
-# echo "USE_PYELPA=${USE_PYELPA}"
-# mpirun -n 4 python -m pytest -s -x tests
+mpirun -n 4 python -m pytest -s tests
