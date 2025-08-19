ea6da1e6cfe4cfc470ba3c9b69cc1135a2e170a3
Pepijn Demol <pepijn.demol@ulb.be>
2025-08-19 13:49:27 +0200

    Update integration test FAM_t0t3
     - update the reference strength to the benchmarked version FAM
     - the strength is sensitive to the tiniest changes to the HF state
       and can differ on a run by run basis. This is probably an artifact
       of the small model space [ => to be investigate ]. This problem
       presently mitigated by specifying the heavy-ball parameters dt,mu
       to get rid of associated randomness in the MOCCa solver, making HF
       and the subsequent FAM reproducible.

--- testing/integration/fam_t0t3.sh
+++ testing/integration/fam_t0t3.sh
@@ -1,19 +1,20 @@
 #-------------------------------------------------------------------------------
-# /!\ NOTE :
-#    This test is presently invalid since the FAM algorithm yields results 
-#    which differ run by run ! 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Perform a spherical HF + FAM calculation of O16 with t0t3 in a minimal box and 
 # compare the Q_20 strength @ 25 MeV.
 # This test may not immediately work for you, since it runs an executable called 
 # Tantalus.LO.master.exe which is a LO compilation of the master branch, required 
 # to be able to read in .wf file for the subsequent FAM calculation. 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# note : the FAM code is sensitive to tiny changes in the reference state. Hence,
+#        HF is converged up to high precision (E_prec = 1e-16). In addition, the
+#        heavy-ball parameters (dt, mu) are fixed to ensure reproducability. Not
+#        doing so would lead to unstable strengths differing from run to run.  
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # This script tests:
 #  Quantity                              Target                     Tolerance
 #  --------                              ------                     ---------
 #  - total HF energy                -177.062001 MeV                  1 keV
-#  - strength S_20 @ 25.0 MeV          1.653682 fm^4 MeV^-1          0.001
+#  - strength S_20 @ 25.0 MeV          1.665536 fm^4 MeV^-1          1e-6
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Useage
 # ------
@@ -22,30 +23,30 @@
 # Dependencies: none
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Owner                : P. Demol [pepijn.demol@ulb.be]
-# Reference commit hash: fcbfa3c08956ff621ee58b6256a3cdd0ec14f270 
+# Reference commit hash: cd78f02f 
 #-------------------------------------------------------------------------------
 # These are the hardcoded answers
 refE=-177.062001 # Total energy of O16 in MeV
-refS20=1.5767491  # Q_20 strength of O16 at 25 MeV in fm^4 MeV^-1  
+refS20=1.665536  # Q_20 strength of O16 at 25 MeV in fm^4 MeV^-1  
 # /!\ : this reference value is invalid as it differs on a run by run basis. 
 
 
 # set -e # exit immediately if command gives non-zero exit status => disabled
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Basic starting point of all testing scripts
 source ../functions.sh
 
 # Set up
 setup_test_env_fam "fam_t0t3" "LO.master" "LO-T" "t0t3"
 
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # (1) Run the mean-field calculation
 
 # Create runtime data
 cat << EOF > mf.data
 &nucleus
 neutrons=8, protons=8
-energy_prec=1e-12
+energy_prec=1e-16
 /
 &mesh
 nx=8, ny=8, nz=8, dx=0.8
@@ -54,14 +55,17 @@ nx=8, ny=8, nz=8, dx=0.8
 name_param='t0t3'
 /
 &pairing
+type='HF'
 /
 &evolution
 maxiter=1000
+dt=0.0209, momentum=0.5746
+Estimateparams=.false.
 /
 &scfiteration
 /
 &wfs
-nwn = 12, nwp = 12
+nwn = 14, nwp = 14
 osc_freq = 0.2, 0.2, 0.2
 /
 &IO
@@ -79,7 +83,7 @@ EOF
 # .... and immediately check if Tantalus reported back some error codes
 tantalus_check=$?
 
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # (2) Run the FAM calculation
 
 # Create runtime data
@@ -101,7 +105,7 @@ maxiter=1000
 &scfiteration
 /
 &wfs
-nwn = 24, nwp = 24
+nwn = 28, nwp = 28
 /
 &IO
 InputFilename='mf.wf'
@@ -127,7 +131,7 @@ EOF
 # .... and immediately check if Tantalus reported back some error codes
 fam_check=$?
 
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Starting the checking
 # a) Get the total energy from the STDOUT file
 E=$(get_total_energy_stdout $mfoutfile)
@@ -137,22 +141,23 @@ check_energy=$?
 
 # b) Get the strength from the S_20.fam file
 S=$(get_strength "S_20.fam" 25.0)
-# ... and compare with a tolerance of 0.001 to the expected answer
-compare_floats $S $refS20 0.001
+
+# ... and compare with a tolerance of 1e-6 to the expected answer
+compare_floats $S $refS20 0.000001
 check_strength=$?
 
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # remove working directory and traces of these calculations
 teardown_test_env
 
-#- - - - - - - - - - - - - -  -- - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Return exit code 1 if any of the checks failed
 fail=$(($tantalus_check || $fam_check || $check_energy || $check_strength ))
 
 if (($fail == 0)) ; then
-	echo "success"
+	echo -e "test FAM t0t3 :\033[1;32m success \033[0m"
 else
-	echo "failed !  tant : $tantalus_check, fam : $fam_check, E_hf : $check_energy, S20 : $check_strength"
+	echo -e "test FAM t0t3 :\033[1;31m failed ! tant : $tantalus_check, fam : $fam_check, E_hf : $check_energy, S20 : $check_strength \033[0m"
 fi
 
 exit $fail
