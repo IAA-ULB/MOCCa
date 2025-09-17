b0618ad7f1fd7c37f77b7190e26baff07bfd1539
Wouter Ryssens <wouter.ryssens@ulb.be>
2025-09-17 16:33:50 +0200

    Fixed circular dependency between functional.f90 and fission_MOI.f90
     Also corrected missing ','s in format statements.

--- src_orig/functional.f90
+++ src_orig/functional.f90
@@ -2594,6 +2594,143 @@ $READPOTENTIALS_HDF5
 
   end function CompStabilisingFactor
 
+  subroutine verify_COM_motion()
+    !---------------------------------------------------------------------------
+    !  This routine performs calculations of the collective moments of inertia
+    !  for the motion of the z-coordinate of the center of mass.
+    !
+    !  The collective coordinate for species q is thus
+    !    Q_q = z_q / A
+    !  to which corresponds a collective momentum  (in our convention)     
+    !    P_q = - i \nabla_z (*)
+    !  
+    !  One can show that, analytically, the collective inertia associated with 
+    !  movement of the centre-of-mass should be the TOTAL mass of the nucleus
+    ! 
+    !   M'_{0} = A m
+    !
+    !  This is what is always presented in the literature. Note however the 
+    !  little accent M', indicating that this IS NOT the collective inertia
+    !  associated with P_q. Rather, it is the collective mass associated with 
+    !     P'_q = hbar P_q
+    !  i.e. the 'physical' convention for the momentum. 
+    !
+    ! We calculate the collective inertia related to COM motion here in multiple
+    ! ways:
+    !
+    !   (a) Analytically, printed as 'A m'
+    !   (b) By using the Belyaev formula for the momentum in our convention (*)
+    !       (and multiplying by hbar^2 afterward)
+    !   (c) By using the perturbative cranking formula starting from Q_q
+    !       (and converting convention again afterward)
+    !
+    ! The results of (b) and (c) are not necessarily close to (a) however: 
+    ! for typical Skyrme interactions the effective mass m^*/m is not equal
+    ! to one, spoiling the correspondence of the perturbative formulation. 
+    ! The origin lies in the absence of Galileian invariance of the interaction 
+    ! in the perturbative treatment, see 
+    !
+    !    K. Wen, and T. Nakatsukasa,  http://arxiv.org/abs/2112.13317
+    ! 
+    ! for a discussion. Ideally, we would calculate an "average" effective mass
+    ! as these authors do and use it to correct our results.
+    !
+    !---------------------------------------------------------------------------
+    
+    use fission_MOI
+    use densities
+    
+    1 format (' Pushing model                 M_0 (MeV/c^2)')
+    2 format (25x, 'neutrons        protons          total', / &
+    &         1x, 80('-'))
+    3 format ('   Belyaev M_0      | ', 3f16.5)
+    4 format ('   Pert. cranking   | ', 32x,f16.5)
+    7 format (1x, 80('-'),/, &
+    &         '   Am               | ', 3f16.5, /, 80('-'))
+    
+    real(KIND=dp), allocatable :: NablaMElements(:,:,:,:)
+    real(KIND=dp) :: mat(2,2), Pmat(2,2), totalmass
+      
+    real(KIND=dp) :: Psp(nwt,nwt), Qsp(nwt,nwt), hbar
+    real(KIND=dp) :: P20(nwt,nwt), Q20(nwt,nwt)
+    
+    ! Calculate hbar to make its use consistent
+    hbar =  sqrt(hbm(1) * 2  * 0.5 * sum(nucleonmass))
+    !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ! Calculate single-particle matrix elements of nabla_z 
+    NablaMElements = compNablaMElements()
+    Psp            =  NablaMElements(3,1,:,:) 
+    if(pairingtype.eq.2) then
+      ! Attention, in a HFB calculation these are the matrix elements in the 
+      ! canonical basis, while Bogoliubov refers to the HF basis. So, we 
+      ! transfer back to the HFbasis
+      Psp = matmul(matmul(cantransfo, Psp), transpose(cantransfo))
+    endif
+    
+    Psp(1:nwn,1:nwn)         =  Psp(1:nwn,1:nwn)         * hbar
+    Psp(nwn+1:nwt,nwn+1:nwt) =  Psp(nwn+1:nwt,nwn+1:nwt) * hbar
+        
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ! Calculate single-particle matrix elements of z-c.o.m. coordinate
+    Qsp            = sqrt(4*pi/3) * Qlm_spme(1,0,.false.) * 10 
+                    ! Q10 = sqrt(3/4pi) * z 
+                    ! and the routine Qlm_spme uses units of b^1/2 => factor 10
+    Qsp(1:nwn,1:nwn)         =      Qsp(1:nwn,1:nwn)        /(neutrons+protons)
+    Qsp(nwn+1:nwt,nwn+1:nwt) =      Qsp(nwn+1:nwt,nwn+1:nwt)/(protons +neutrons)
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ! TODO: Calculate the average effective mass for the pushing model.
+    !       This used to work, but now fails thanks to the changes of the 
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ! Calculate average effective mass 
+    !
+    !   m^*/m_q  = 2m/N_q * int d^3r   rho_q(r) hbar^2/2m^*_q(r)
+    ! 
+    ! with hbar^2/2m^*_q =  hbar^2/2m_q(r) + F_Nm_Nm(r)
+    ! 
+    ! which is the logical generalization from Eq. (30) in 
+    !    K. Wen, and T. Nakatsukasa,  http://arxiv.org/abs/2112.13317
+    ! and an explicit factor of hbar^2.
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    !     avg_effmass = sum(F%F_Nm_Nm(:,:) * R%D_I_I(:,:) , 1) * dv
+    !     avg_effmass(1) = avg_effmass(1) / (hbm(1) * neutrons)
+    !     avg_effmass(2) = avg_effmass(2) / (hbm(2) * protons)
+    !     avg_effmass    = avg_effmass + 1
+    !     avg_effmass    = 1/avg_effmass
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print 1
+    print 2
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ! perform the summations
+    select case(pairingtype)
+    case(0) ! HF
+      Pmat                     =  Ksum_Mij_HF(Psp, Psp, 1, 1,(/1,3/))
+      mat                      =  Ksum_Mij_HF(Qsp, Qsp, 1, 1,(/1,3/))
+    case(1) ! BCS
+      Pmat                     =  Ksum_Mij_BCS(Psp, Psp, 1, 1,(/1,3/))
+      mat                      =  Ksum_Mij_BCS(Qsp, Qsp, 1, 1,(/1,3/))
+    case(2) !HFB
+      P20 = calc_Q20(Psp, bogoliubov, 1)        
+      Pmat                     =  Ksum_Mij(P20, P20, 1, 1,(/1,3/))
+      Q20 = calc_Q20(Qsp, bogoliubov, 1)        
+      mat                      =  Ksum_Mij(Q20, Q20, 1, 1,(/1,3/))
+    end select
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    ! Calculating the inertia parameters
+    totalmass   = 1.0d0/(sum(mat(1,:))) * sum(mat(2,:)) * 1.0d0/(sum(mat(1,:)))
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ! Printing 
+    print 3,  Pmat(1,:), sum(Pmat(1,:))
+    print 4,  totalmass * hbar**2
+    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ! Analytical result
+    print 7,  neutrons * nucleonmass(1), protons*nucleonmass(2), &
+    &         neutrons * nucleonmass(1)+ protons*nucleonmass(2) 
+
+    print *
+
+  end subroutine verify_COM_motion
+
 #if( $FAM == 0)
   function PVectorInproduct(F1, F2) result(x)
     !---------------------------------------------------------------------------
