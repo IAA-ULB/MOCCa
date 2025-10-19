43b7d23a95cf8fc5d2a5f6068c5a559f0bbf6a19
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-10-19 14:24:50 +0200

    Comment on scripts/ca40_v2.py

--- MOCCaPy/scripts/ca40_v2.py
+++ MOCCaPy/scripts/ca40_v2.py
@@ -1,21 +1,28 @@
 # A second attempt at MOCCaPy
+### opmerkingen van mij beginnen met `###`
 from mocca.param      import Param
-### I strongly recommend honouring PEP 8 (https://peps.python.org/pep-0008/)
 from mocca            import mfstate # mfstate =  short for mean-field state
-from mocca.densities  import densityvector ### DensityVector?
+from mocca.densities  import densityvector
 from mocca.potentials import potentialvector
-### class names must start with capital (typically CamelCase)
-### methods start with lowercase
+### I strongly recommend honouring PEP 8 (https://peps.python.org/pep-0008/)
+###     module names in lowercase
+###     class names must start with capital (typically CamelCase)
+###     methods start with lowercase
+### names should be descriptive
+### names of functions should be a verb(+noun) in it because they DO sth (to/with noun)
+### names of objects should be nouns because they represent objects
+### This helps the user understand what he is importing
 
 from mocca.mesh   import Cartesian3D
 ### The name Cartesian3D does not cover its meaning: it misses that it is in fact a Lagrange mesh
 
 from mocca.EDFs   import BXL
 # from mocca.edf import BXL ### module names in lowercase
-### overbodid: Param objegt genereert BXL automatisch
+### overbodig: Param instance genereert automatisch een BXL object op basis van func_file='BXL.func"
+### dat is al geimplementeerd
 
-from mocca.evolve import heavy_ball ### = fungtie?
-from mocca.scf    import linear_mix ### = fungtie?
+from mocca.evolve import heavy_ball ### = functie?
+from mocca.scf    import linear_mix ### = functie?
 from mocca.io     import write_mf_hdf5
 
 # Define a Cartesian mesh with antiperiodic boundary conditions
@@ -28,12 +35,15 @@ mesh = Cartesian3D(n=(16,16,16), d=(0.8,0.8,0.8), bc='antiperiodic' )
 # - initialized with an analytical Nilsson s.p. hamiltonian (alternative is random initialisation)
 # - (by default) maximally symmetric
 # - in a modelspace defined by 40 neutron and 40 proton spwfs                     NOTE: should modelspace be separated as a concept?
-wf = mfstate(Z=20,N=20,iniwfs='Nilsson', mesh=mesh, type='Slater',nwn=40,nwp=40)
+wf = mfstate(Z=20, N=20, iniwfs='Nilsson', mesh=mesh, type='Slater', nwn=40, nwp=40)
 ### Het Nucleus concept is afgevoerd?
-### mfstate is een functie? of een object?
-### names of functions should be a verb(+noun) in it because they DO sth (to/with noun)
-### namss of objects should be nouss because they represent objects
+### mfstate is een functie? of een klasse waarvan hier de constructor wordt opgeroepen?
 ### noch mfstate, noch wf worden hieronder gebruikt?
+### Mij lijkt het dat een (mean field) state een instance van een klasse is vermits state een noun is,
+### Misschien moet je me even uitleggen wat het concept mean-field state juist inhoudt.
+### ik vind de namen verwarrend. Is een mfstate een golf-functie?
+### type='Slater' suggereert dat het misschien logischer zou zijn om iets te doen in de aard van
+wf = SlaterDeterminant(Z=20, N=20, iniwfs='Nilsson', mesh=mesh, nwp=40, nwn=40)
 
 # Define an EDF ....
 param = Param("BSkG1") # read BSkG1.param file from mocca/parametrezations (a soft link to tantalus_full/parametrizations
@@ -50,6 +60,7 @@ bxl = param.create_EDF() ### the name bxl is arbitrary, but hints at it being a 
 ### A different representation may be useful when the Fortran executable is phased out in the future.
 
 # set some convergence_criteria
+### misschien ook beter een klasse voor maken. de constructor is een handige plaats voor default values
 cc = {
         'energy_precision' : 1e-9  # in units of MeV
         'moment_precision' : 1e-3  # dimensionless
@@ -57,9 +68,15 @@ cc = {
         # those that are not specified explicitly should be set to sensible defaults
      }
 # ... and then perform the evolution until convergence is reached; only at this point is an EDF actually needed
-### OOPS evolve is not knowne... (as are none of the methods used belowe
+### evolve is not known... (as are none of the methods used below)
+### wat doet evolve precies? Het is een verb dat (in mijn gedachten) een tijdsevolutie berekend...
+### names should be descriptive - al je huidige gebruikers weten ongetwijfeld wat daarmee bedoeld
+### wordt, maar nieuwe gebruikers worden beter niet op het verkeerde been gezet. Een van de basisprincipes van Python
+### is het principle of least surprise. Hoe beter een naam aangeeft wat er gebeurt, hoe minder verrassingen en hoe
+### minder steil de leercurve is.
 wf, R, F = evolve(strategy=heavy_ball, scf=linear_mix, EDF, param, convergence_criteria = cc)
 wf, R, F = evolve(strategy=heavy_ball, scf=linear_mix, edf=bxl, convergence_criteria = cc)
+### strategy en scf zijn misschien iets te vage namen.
 #               |> this chooses a strategy to evolve the single-particle wavefunctions
 #               |> this chooses a strategy to evolve the single-particle potentials
 #               |> this chooses the EDF form to use
@@ -68,38 +85,47 @@ wf, R, F = evolve(strategy=heavy_ball, scf=linear_mix, edf=bxl, convergence_crit
 # Additional optional arguments to evolve could be
 #   - R0: a density-vector to start from; if not provided, it should be one calculated from wf
 #   - F0: a potential-vector to start from; if not provided, it should be calculated from wf
-
-### questions
-### wf (van wf = mfstate(...) hierboven) wordt hier overschreven? maar niet eerder gebruikt
+### moet wf hier ook geen argument zijn?
+### wf (van wf = mfstate(...) hierboven) wordt hier overschreven? maar werd nergens gebruikt
 
 # After the evolution
 # - wf is the final solution
 # - R the vector of densities that corresponds to it; an instance of the densityvector class
 # - F the vector of potentials that corresponds to it; an instance of the potentialvector class
+### wf vind ik acceptable als naam, R en F niet. om te ze zijn niet lowercase en niet descriptief
 #
 # There is however, an unsolved problem here: the precise realisation of DensityVector and PotentialVector depend on the EDF selected.
 ### dat is het leuken van heet dynamische karakter van Python. Objecten kunnen veranderen. Zolang de EDF in kwestie weet
 ### hoe DensityVector/PotentialVector gemaakt moet worden is er geen probleem. De verantwoordelijkheid van EDFs wordt
 ### daarmee wel groter maar het is logisch dat als B afhangt van A, dat A dan ook weet hoe, en daar verantwoordelijk
 ### voor is.
-### Wat bedoel je precies met een vector van densities/potentials hier?
+### Wat bedoel je precies met een vector van densities/potentials hier? zijn dat scalaire functies gediscretiseerd op 
+### het mesh? zijn dat  echte vectoren of gaat het eerder om een collectie van densities/potentials?
 
 # Calculate some observables and print them
 # NOTE: I'm unsure if we should make the effort to save some CPU cycles and not recalculate things that are likely to have
 # been calculated during the call to evolve
 E = calculate_energy(wf,EDF,param)   # here the EDF and parameterisation are again needed;
                                    # you might conceivably want to calculate the energy wf with another model
-Espwf = calculate_spwf_energy(wf,F,EDF,param) # alternate way to calculate the energy; potentials are required
+E = calculate_energy(wf,bxl)   ### param is an attribute of bxl
+Espwf = calculate_spwf_energy(wf, F, EDF,param) # alternate way to calculate the energy; potentials are required
+Espwf = calculate_spwf_energy(wf, F, bxl) ### param is an attribute of bxl
 Qlm = calculate_multipole_moments(wf) # Qlm is a dictionary or list with values for all multipole moments
 # ..... + many others + .....
 ### die functies moeten nog geimporteerd worden.
-print *, 'Total energy (from functional)', E
-print *, 'Total energy (from spwfs)', Espwf
-print *, 'Difference', E-Espwf     # Should be very small when converged
-
+print('Total energy (from functional)', E)
+print('Total energy (from spwfs)', Espwf)
+print('Difference', E-Espwf)     # Should be very small when converged
 
 # ... and write the mean-field state to file
 write_mf_hdf5(wf,R,F0)
+### hdf5file openen en elk van de object apart wegschrijven
+import h5py
+hdf5file = h5py.File('myfile.hdf5','w')
+wf.write_hdf5(hdf5file)
+R.write_hdf5(hdf5file)
+F0.write_hdf5(hdf5file)
+
 # NOTE
 #  - this function should take care of meta-data related to wf
 #  - I'm undecided whether observables should be stored
