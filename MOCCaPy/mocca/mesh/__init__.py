caa98417c5c03a1a9fb60594e60f9165994dfb64
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-10-20 00:36:25 +0200

    Fix tests/mocca/mesh/test_mesh.py

--- MOCCaPy/mocca/mesh/__init__.py
+++ MOCCaPy/mocca/mesh/__init__.py
@@ -1,23 +1,33 @@
 class LagrangeMesh:
-    def __init__(self, n: int|tuple, d: int|float|tuple, dim: int=3, bc='antiperiodic') -> None:
+    def __init__(self, n: int|tuple, d: int|float|tuple, dim: int=0, bc='antiperiodic') -> None:
+        """Construct a Lagrange mesh in 1, 2 or 3 dimensions.
 
-        assert 1 <= dim <= 3
-        self.dim = dim
+        Args:
+            dim (int): dimension of mesh. if 0, dim is guessed as len(n)
+            n: number of points in the respective dimensions. If n is an int n is the same in each direction.
+            d: spacing of points in the respective dimensions. If n is a float or an int d is the same in each direction.
+            bc: boundary condition type. 'antiperiodic' or 'periodic'.
+        """
+        if dim == 0 and isinstance(n, tuple):
+            self.dim = len(n)
+        else:
+            self.dim = dim
 
-        # dimension of the grid (nx,ny,nz) (which applies to 1/8 of the box?)
+        assert 1 <= self.dim <= 3
+
         if isinstance(n, int):
-            self.n = tuple(dim*[n])
+            self.n = tuple(self.dim*[n])
         else:
             assert isinstance(n, tuple)
-            assert len(n) == dim
+            assert len(n) == self.dim
             for ni in n:
                 assert isinstance(ni, int)
             self.n = n
 
         # box spacing (ints are converted to floats)
         if isinstance(d,tuple):
-            assert len(d) == 3
+            assert len(d) == self.dim
             self.d = tuple(float(di) for di in d)
         else:
             df = float(d)
-            self.d = tuple(dim*[float(d)])
+            self.d = tuple(self.dim*[float(d)])
--- MOCCaPy/tests/mocca/mesh/test_mesh.py
+++ MOCCaPy/tests/mocca/mesh/test_mesh.py
@@ -1,22 +1,98 @@
 import pytest
 
-from mocca.mesh import Cartesian3D
+from mocca.mesh import LagrangeMesh
 
-def test_Cartesian3D_constructor():
-    mesh = Cartesian3D(n=5, d=.5)
-    assert mesh.n == (5, 5, 5)
+def test_LagrangeMesh_constructor():
+    # valid 1D mesh constructions
+    mesh = LagrangeMesh(dim=1, n=5, d=.5,)
+    assert mesh.dim == 1
+    assert mesh.n == (5,)
+    assert mesh.d == (.5,)
 
-    mesh = Cartesian3D(n=(3,4,5), d=.5)
-    assert mesh.n == (3, 4, 5)
+    mesh = LagrangeMesh(n=(5,), d=.5,)
+    assert mesh.dim == 1
+    assert mesh.n == (5,)
+    assert mesh.d == (.5,)
+
+    # valid 2D mesh constructions
+    mesh = LagrangeMesh(dim=2, n=5, d=.5,)
+    assert mesh.dim == 2
+    assert mesh.n == (5,5)
+    assert mesh.d == (.5,.5)
+
+    mesh = LagrangeMesh(dim=2, n=5, d=(.5,.6))
+    assert mesh.dim == 2
+    assert mesh.n == (5,5)
+    assert mesh.d == (.5,.6)
+
+    mesh = LagrangeMesh(n=(5,6), d=.5)
+    assert mesh.dim == 2
+    assert mesh.n == (5,6)
+    assert mesh.d == (.5,.5)
+
+    mesh = LagrangeMesh(n=(5,6), d=(.5,.6))
+    assert mesh.dim == 2
+    assert mesh.n == (5,6)
+    assert mesh.d == (.5,.6)
+
+    # valid 3D mesh constructions
+    mesh = LagrangeMesh(dim=3, n=5, d=.5)
+    assert mesh.dim == 3
+    assert mesh.n == (5,5,5)
+    assert mesh.d == (.5,.5,.5)
+
+    mesh = LagrangeMesh(dim=3, n=(4,5,6), d=.5,)
+    assert mesh.dim == 3
+    assert mesh.n == (4,5,6)
+    assert mesh.d == (.5,.5,.5)
+
+    mesh = LagrangeMesh(dim=3, n=5, d=(.4,.5,.6))
+    assert mesh.dim == 3
+    assert mesh.n == (5,5,5)
+    assert mesh.d == (.4,.5,.6)
+
+    mesh = LagrangeMesh(n=(4,5,6), d=(.4,.5,.6))
+    assert mesh.dim == 3
+    assert mesh.n == (4,5,6)
+    assert mesh.d == (.4,.5,.6)
+
+    mesh = LagrangeMesh(n=(4,5,6), d=.5)
+    assert mesh.dim == 3
+    assert mesh.n == (4,5,6)
+    assert mesh.d == (.5,.5,.5)
+
+    # Invalid 1D mesh constructions
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(n=5, d=.5)
+        # although this could be interpreted as a 1D mesh, the user might expect a 3D mesh.
+        # The user must either provide dim=1 or n=tuple(5,)
 
     with pytest.raises(AssertionError):
-        mesh = Cartesian3D(n=(3,4), d=.5)
+        mesh = LagrangeMesh(dim=1, n=5, d=(.5,.5))
 
-    mesh = Cartesian3D(n=5, d=5)
-    assert mesh.d == (5., 5., 5.)
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(dim=1, n=(5,6), d=.5)
 
-    mesh = Cartesian3D(n=5, d=.5)
-    assert mesh.d == (.5, .5, .5)
+    # Invalid 2D mesh constructions
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(n=(4,5), d=(.4,.5,.6))
 
-    mesh = Cartesian3D(n=5, d=(.6,.8,1))
-    assert mesh.d == (.6, .8, 1.0)
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(dim=2, n=(4,5,6), d=(.4,.5))
+
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(dim=2, n=5, d=(.4,.5,.6))
+
+    # Invalid 3D mesh constructions
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(n=(4,5), d=(.4,.5,.6))
+
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(dim=3, n=5, d=(.5,.6))
+
+    # Invalid 4D mesh constructions
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(dim=4, n=5, d=.5)
+
+    with pytest.raises(AssertionError):
+        mesh = LagrangeMesh(n=(4,5,6,7), d=.5)
