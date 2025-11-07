Fix D1 computation (non-reduced axes only)
Apparently, there was a small (sign) error in Ryssens et al., 2015 eq 18 and 19., see MOCCaPy/mocca/mesh/lagrange.md, (eqs 10.1, 10.2 and the warning following them)

MOCCaPy/mocca/mesh/lagrange.py
@@ -472,19 +472,32 @@ class LagrangeMesh:
             alternating_sign = np.empty((ng+1), dtype=float)
             alternating_sign[::2] = 1
             alternating_sign[1::2] = -1
-            i = np.linspace(0, ng-1, ng)
-            D1 = np.empty((ng,ng), dtype=float)
+            # alternating_sign = [1, -1, 1, -1, ...]
+            # In agreement with the Warning following equations 10.1 and 10.2 in lagrange.md, which demands
+            # an extra sign flip because they use (i-j) in the sine argements instead of (j-i)
+            # i = np.linspace(0, ng - 1, ng)
+            minus_i = -np.linspace(0, ng - 1, ng)
+            D1 = np.empty((ng,ng), dtype=float) # deliberately not order='F' for performance reasons
+            # In agreement with the formulas in Ryssens et al 2015 (eq 18),
+            # j is the row index and i the column index (D_ji)
             for j in range(ng):
-                row = i-j
+                # row = i-j
+                row = minus_i+j # j-i
                 row *= (np.pi / ng) # ng = 2N
                 row[j] = .1 # avoid division by 0 in row[j] below
                 row = (np.pi / (ng * self.d[axis])) / np.sin(row)
                 # division by 0 in row[j] yields `inf`, to be replaced by 0, (l'Hopitals rule)
                 row[j] = .0
                 if j % 2 == 0: # j is even
-                    row = row * alternating_sign[1:]
-                else:          # j is odd
+                    # flip the sign of entries 1 3 5 ... i.e. those with odd i
+                    # since j is even, i-j is odd iff i is odd: (-1)^(i-j) is -1 for odd i
                     row = row * alternating_sign[:ng]
+                    # assert alternating_sign[0] == 1 # True by construction
+                else:          # j is odd
+                    # flip the sign of entries 0 2 4 ... i.e. those with even i
+                    # since j is odd, i-j is odd iff i is even  and (-1)^(i-j) is -1 for even i
+                    row = row * alternating_sign[1:]  # flip sign of entries 0 2 4 ...
+                    # assert alternating_sign[1] == -1  # True by construction
                 D1[j, :] = row
         else:
             D1 = None

MOCCaPy/tests/mocca/mesh/test_Observable.py
@@ -39,13 +39,16 @@ def dbf_dx(mesh, i, r, i_sign=1):
                               mesh.gx[i] * i_sign
                             ) / (mesh.box_width[0] * mesh.d[0])
 
-    factor = np.sqrt(1. / mesh.box_width[0])
+    factor = 1. / np.sqrt(mesh.box_width[0])
 
     arg = two_pi_K * r
     result = np.empty((r.shape[0], 2), dtype=float, order='F')
     result[:, 0] = -np.sin(arg)
     result[:, 1] =  np.cos(arg)
-    result *= (factor*two_pi_K)
+    result *= factor
+    # By leaving out the factor two_pi_K, the derivative has the same
+    # y-span as the basis function
+    result *= -1 if two_pi_K < 0 else 1
 
     return result
 
@@ -68,77 +71,98 @@ def test_differentiate_1D_x(debug=False):
         mesh = LagrangeMesh(dim=1, M=2*N, d=d, reduced=reduced, highest_derivative_order=1)
         D1 = mesh.D[0,0]
         pw = mesh.basis_function(0, r)
+        two_pi_K = abs(2. * np.pi * mesh.gx[0]) / (mesh.box_width[0] * mesh.d[0])
 
+        # validate D1 against a literal coding of the Ryssens et al 2015 eq 18
         for i in range(2*N):
             for j in range(2*N):
                 if i == j:
                     assert D1[j,i] == 0
                 else:
-                    d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(i-j)/(2*N)))
+                    # d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(i-j)/(2*N)))
+                    d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(j-i)/(2*N)))
                     print(f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}")
-                    # assert D1[j,i] == pytest.approx(d1ji), f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}"
+                    assert D1[j,i] == pytest.approx(d1ji), f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}"
 
         fig, ax = plt.subplots()
         plt.title(f'{0}, x_i={mesh.gx[0]}')
         ax.plot([-N,N],[0,0],color='k')
         ax.plot([0,0],[-1,1],color='k')
         ax.plot(mesh.gx,  2*N*[0], 'rs')
-        ax.plot(mesh.gx[0], [0], 'gs')
+        ax.plot(mesh.gx[0], [0], 'gs') # mark the mesh point to which the basis function corresponds
         ax.plot(r, pw[:,0], label='real')
         ax.plot(r, pw[:,1], label='imag')
 
         Q = Observable(mesh, data = mesh.basis_function(ijk=0, r=mesh.gx))
+        # check Q against bf() above
         Q_expected = bf(mesh, i=0, r=mesh.gx)
-        for i in range(2*N):
-            print(f"{Q.data[i,0]} {Q_expected[i,0]} {Q.data[i,1]} {Q_expected[i,1]} ")
-        Qx = Q.differentiate(axes='x')
+        ax.plot(mesh.gx, Q_expected[:,0], 'bo', label='bf real')
+        assert Q.data == pytest.approx(Q_expected)
+        # for i in range(2*N):
+        #     print(f"{Q.data[i,0]} {Q_expected[i,0]} {Q.data[i,1]} {Q_expected[i,1]} ")
+        dQdx = Q.differentiate(axes='x')
 
-        DQ0 = np.zeros((6,),dtype=float)
-        for i in range(2*N):
-            for j in range(2*N):
-                DQ0[i] += D1[i,j] * Q.data[j,0]
-        # for i in range(6):
-        #     DQ0[i] = np.sum(D1[i,:] * Q.data[:,0])
+        # manually perform dQdx = D1 * Q.data - real part
+        DQR = np.zeros((2*N,), dtype=float)
+        for i in range(2*N): # loop over the rows of D1
+            for j in range(2*N): # scalar product of D[i,:] and Q.data[:,0]
+                DQR[i] += D1[i,j] * Q.data[j,0]
 
         for i in range(2 * N):
-            print(f"{i=}: {DQ0[i]} {Qx[i,0]}")
-            assert DQ0[i] == pytest.approx(Qx[i,0]), f"{i=}: {DQ0[i]=}, {Qx[i,0]=}"
+            print(f"{i=}: {DQR[i]} {dQdx[i,0]}")
+            assert DQR[i] == pytest.approx(dQdx[i,0]), f"{i=}: {DQR[i]=}, {dQdx[i,0]=}"
 
-        ratio = Qx[:, 0] / Q.data[:, 1]
-        print(f"QxR/QI={Qx[:, 0] / Q.data[:, 1]}")
-        for rt in ratio:
-            assert rt == pytest.approx(ratio[0])
+        # manually perform dQdx = D1 * Q.data - imag part
+        DQI = np.zeros((2*N,),dtype=float)
+        for i in range(2*N): # loop over the rows of D1
+            for j in range(2*N): # scalar product of D[i,:] and Q.data[:,1]
+                DQI[i] += D1[i,j] * Q.data[j,1]
 
-        ratio = Qx[:, 1] / Q.data[:, 0]
-        print(f"QxI/QR={ratio}")
-        for rt in ratio:
-            assert rt == pytest.approx(ratio[0])
+        for i in range(2 * N):
+            print(f"{i=}: {DQI[i]} {dQdx[i,1]}")
+            assert DQI[i] == pytest.approx(dQdx[i,1]), f"{i=}: {DQI[i]=}, {dQdx[i,1]=}"
 
-        Qx_expected = dbf_dx(mesh, i=0, r=mesh.gx)
-        for i in range(6):
-            print(f"{i=}: real {Qx[i,0]} {Qx_expected[i,0]}")
-            assert Qx[i, 0] == pytest.approx(Qx_expected[i, 0])
+        # check dQdx against dbf_dx
+        dQdx_expected = dbf_dx(mesh, i=0, r=mesh.gx)
+        ax.plot(mesh.gx, dQdx_expected[:, 0], 'yo--', label='dbf/dx real')
+        ax.plot(mesh.gx, dQdx_expected[:, 0]*two_pi_K, 'yo', label='dbf/dx real*')
 
-            print(f"{i=}: imag {Qx[i,1]} {Qx_expected[i,1]}")
-            assert Qx[i, 1] == pytest.approx(Qx_expected[i, 1])
+        # ax.plot(mesh.gx, dQdx_expected[:,1], '--', label='dbf/dx imag')
+        for i in range(2*N):
+            print(f"{i=}: real {dQdx[i,0]} {dQdx_expected[i,0]}")
+            assert dQdx[i, 0] == pytest.approx(two_pi_K*dQdx_expected[i, 0])
+
+            print(f"{i=}: imag {dQdx[i,1]} {dQdx_expected[i,1]}")
+            assert dQdx[i, 1] == pytest.approx(two_pi_K*dQdx_expected[i, 1])
+
+        ratio = dQdx[:, 0] / Q.data[:, 1]
+        print(f"dQdxR/QI={dQdx[:, 0] / Q.data[:, 1]}")
+        # assert ratio[0] > 0
+        # for rt in ratio:
+        #     assert rt == pytest.approx(ratio[0])
+
+        ratio = dQdx[:, 1] / Q.data[:, 0]
+        print(f"dQdxI/QR={ratio}")
+        # assert ratio[0] < 0
+        # for rt in ratio:
+        #     assert rt == pytest.approx(ratio[0])
+
 
         # ax.plot(mesh.gx, Q_expected[:,0], 'b--')
         # ax.plot(mesh.gx, Q_expected[:,1], 'r--')
 
-        ax.plot(mesh.gx, Qx_expected[:,0], 'y--', label='dbf/dx real')
-        ax.plot(mesh.gx, Qx_expected[:,1], '--', label='dbf/dx imag')
 
         # ax.plot(mesh.gx, Q.data[:,0], 'co', label='Q real')
         # ax.plot(mesh.gx, Q.data[:,1], 'c*', label='Q imag')
-        ax.plot(mesh.gx, Qx[:,0], 'yx', label='dQ/dx real')
-        ax.plot(mesh.gx, Qx[:,1], '*', label='dQ/dx imag')
+        ax.plot(mesh.gx, dQdx[:,0], 'yx', label='dQ/dx real')
+        # ax.plot(mesh.gx, dQdx[:,1], '*', label='dQ/dx imag')
 
         plt.legend()
         fig.savefig(png_folder/f"1D_basis_function_{0}_differentiation.png")
         plt.close(fig)
 
         # print(f"Q={Q.data}")
-        # print(f"dQ/dx={Qx}")
+        # print(f"dQ/dx={dQdx}")
 
         print("test_differentiate_1D finished")
 