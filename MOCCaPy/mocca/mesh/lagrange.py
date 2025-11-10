Have LagrangeMesh.basis_function return also its symmetry sign. Fix tests.

MOCCaPy/mocca/mesh/lagrange.py
@@ -438,7 +438,9 @@ class LagrangeMesh:
                 the corresponding entry in ijk_sign to -1.)
                 (ignored for non-reduced axes).
         Returns:
-            Array of values of the basis function at r.
+            (data, symmetry)
+            data: Array of values of the basis function at r.
+            symmetry: the symmetry sign of the basis function (+1 for real components, -1 for imaginary components).
         """
         assert isinstance(r, np.ndarray)
 
@@ -461,6 +463,9 @@ class LagrangeMesh:
                                                ) / (self.box_width[2] * self.d[2])
                                 ], dtype=float, order='F')
             factor = np.sqrt(1/(self.box_width[0] * self.box_width[1] * self.box_width[2]))
+            symmetry = np.array([[1, -1],
+                                 [1, -1],
+                                 [1, -1]], dtype=float, order='F')
 
         elif self.dim == 2:
             assert r.shape[1] == 2
@@ -474,6 +479,8 @@ class LagrangeMesh:
                                                ) / (self.box_width[1] * self.d[1])
                                 ], dtype=float, order='F')
             factor = np.sqrt(1/(self.box_width[0] * self.box_width[1]))
+            symmetry = np.array([[1,-1],
+                                 [1,-1]], dtype=float, order='F')
 
         elif self.dim == 1:
             if len(r.shape) == 1:
@@ -486,6 +493,7 @@ class LagrangeMesh:
                                                ) / (self.box_width[0] * self.d[0])
                                 ], dtype=float, order='F')
             factor = np.sqrt(1 / self.box_width[0])
+            symmetry = [1,-1]
 
         arg = r @ two_pi_K
         result = np.empty((r.shape[0],2), dtype=float, order='F')
@@ -493,7 +501,7 @@ class LagrangeMesh:
         np.sin(arg, out=result[:,1])
         result *= factor
 
-        return result
+        return result, symmetry
 
 
     # ---------------------------------------------------------------------------
@@ -556,8 +564,6 @@ class LagrangeMesh:
         #   stores nothing if the axis is not reduced,
         #   and the reduced matrix difference D-E otherwise.
         self.D[0, 0] = self._compute_D1(0)
-        for i in range(6):
-            print(self.D[0,0][i,:])
         if self.dim > 1:
             if (self.M[1] == self.M[0]) and \
                     (self.d[1] == self.d[0]) and \
@@ -594,10 +600,6 @@ class LagrangeMesh:
                     for icol in range(N):
                         D[:,icol] = self.D[axis, order][N:,N+icol]   # copy Dlr (lower right quadrant)
                         E[:,N-1-icol] = self.D[axis, order][N:,icol] # reverse the order of Dll (lower left quadrant)
-                    for i in range(3):
-                        print(D[i, :])
-                    for i in range(3):
-                        print(E[i, :])
                     self.D  [axis, order] = D + E # replaces the full D matrix
                     self.DmE[axis, order] = D - E
 

MOCCaPy/tests/mocca/mesh/test_Observable.py
@@ -68,102 +68,102 @@ def test_differentiate_1D_x_non_reduced(debug=False):
 
     mesh = LagrangeMesh(dim=1, M=2*N, d=d, reduced=False, highest_derivative_order=1)
     D1 = mesh._get_D(axis=0, order=1)
-    pw = mesh.basis_function(0, r)
+    pw, _ = mesh.basis_function(0, r)
     two_pi_K = abs(2. * np.pi * mesh.gx[0]) / (mesh.box_width[0] * mesh.d[0])
 
-    if reduced == False:
-        # Validate D1 against a literal coding of the Ryssens et al 2015 eq 18
-        # (correcting the sign error in that formula)
-        for i in range(2*N):
-            for j in range(2*N):
-                if i == j:
-                    assert D1[j,i] == 0
-                else:
-                    # d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(i-j)/(2*N))) # (i-j) is wrong
-                    d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(j-i)/(2*N)))   # (j-i) is correct
-                    print(f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}")
-                    assert D1[j,i] == pytest.approx(d1ji), f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}"
+    # Validate D1 against a literal coding of the Ryssens et al 2015 eq 18
+    # (correcting the sign error in that formula)
+    for i in range(2*N):
+        for j in range(2*N):
+            if i == j:
+                assert D1[j,i] == 0
+            else:
+                # d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(i-j)/(2*N))) # (i-j) is wrong
+                d1ji = (-1)**(i-j) * np.pi / (2 * N * d * np.sin(np.pi*(j-i)/(2*N)))   # (j-i) is correct
+                print(f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}")
+                assert D1[j,i] == pytest.approx(d1ji), f"D1[{j},{i}]: {D1[j,i]=}, {d1ji=}, diff={D1[j,i]-d1ji}"
 
-        fig, ax = plt.subplots()
-        plt.title(f'{0}, x_i={mesh.gx[0]}')
-        ax.plot([-N,N],[0,0],color='k')
-        ax.plot([0,0],[-1,1],color='k')
-        ax.plot(mesh.gx,  2*N*[0], 'rs')
-        ax.plot(mesh.gx[0], [0], 'gs') # mark the mesh point to which the basis function corresponds
-        ax.plot(r, pw[:,0], label='real')
-        ax.plot(r, pw[:,1], label='imag')
+    fig, ax = plt.subplots()
+    plt.title(f'{0}, x_i={mesh.gx[0]}')
+    ax.plot([-N,N],[0,0],color='k')
+    ax.plot([0,0],[-1,1],color='k')
+    ax.plot(mesh.gx,  2*N*[0], 'rs')
+    ax.plot(mesh.gx[0], [0], 'gs') # mark the mesh point to which the basis function corresponds
+    ax.plot(r, pw[:,0], label='real')
+    ax.plot(r, pw[:,1], label='imag')
 
-        Q = Observable(mesh, data = mesh.basis_function(ijk=0, r=mesh.gx))
-        # check Q against bf() above
-        Q_expected = bf(mesh, i=0, r=mesh.gx)
-        ax.plot(mesh.gx, Q_expected[:,0], 'bo', label='bf real')
-        assert Q.data == pytest.approx(Q_expected)
-        # for i in range(2*N):
-        #     print(f"{Q.data[i,0]} {Q_expected[i,0]} {Q.data[i,1]} {Q_expected[i,1]} ")
-        dQdx = Q.differentiate(axes='x')
+    mbf, symm_mbf = mesh.basis_function(ijk=0, r=mesh.gx)
+    Q = Observable(mesh, data=mbf, symmetry=symm_mbf)
+    # check Q against bf() above
+    Q_expected = bf(mesh, i=0, r=mesh.gx)
+    ax.plot(mesh.gx, Q_expected[:,0], 'bo', label='bf real')
+    assert Q.data == pytest.approx(Q_expected)
+    # for i in range(2*N):
+    #     print(f"{Q.data[i,0]} {Q_expected[i,0]} {Q.data[i,1]} {Q_expected[i,1]} ")
+    dQdx = Q.differentiate(axes='x')
 
-        # manually perform dQdx = D1 * Q.data - real part
-        DQR = np.zeros((2*N,), dtype=float)
-        for i in range(2*N): # loop over the rows of D1
-            for j in range(2*N): # scalar product of D[i,:] and Q.data[:,0]
-                DQR[i] += D1[i,j] * Q.data[j,0]
+    # manually perform dQdx = D1 * Q.data - real part
+    DQR = np.zeros((2*N,), dtype=float)
+    for i in range(2*N): # loop over the rows of D1
+        for j in range(2*N): # scalar product of D[i,:] and Q.data[:,0]
+            DQR[i] += D1[i,j] * Q.data[j,0]
 
-        for i in range(2 * N):
-            print(f"{i=}: {DQR[i]} {dQdx[i,0]}")
-            assert DQR[i] == pytest.approx(dQdx[i,0]), f"{i=}: {DQR[i]=}, {dQdx[i,0]=}"
+    for i in range(2 * N):
+        print(f"{i=}: {DQR[i]} {dQdx[i,0]}")
+        assert DQR[i] == pytest.approx(dQdx[i,0]), f"{i=}: {DQR[i]=}, {dQdx[i,0]=}"
 
-        # manually perform dQdx = D1 * Q.data - imag part
-        DQI = np.zeros((2*N,),dtype=float)
-        for i in range(2*N): # loop over the rows of D1
-            for j in range(2*N): # scalar product of D[i,:] and Q.data[:,1]
-                DQI[i] += D1[i,j] * Q.data[j,1]
+    # manually perform dQdx = D1 * Q.data - imag part
+    DQI = np.zeros((2*N,),dtype=float)
+    for i in range(2*N): # loop over the rows of D1
+        for j in range(2*N): # scalar product of D[i,:] and Q.data[:,1]
+            DQI[i] += D1[i,j] * Q.data[j,1]
 
-        for i in range(2 * N):
-            print(f"{i=}: {DQI[i]} {dQdx[i,1]}")
-            assert DQI[i] == pytest.approx(dQdx[i,1]), f"{i=}: {DQI[i]=}, {dQdx[i,1]=}"
+    for i in range(2 * N):
+        print(f"{i=}: {DQI[i]} {dQdx[i,1]}")
+        assert DQI[i] == pytest.approx(dQdx[i,1]), f"{i=}: {DQI[i]=}, {dQdx[i,1]=}"
 
-        # check dQdx against dbf_dx
-        dQdx_expected = dbf_dx(mesh, i=0, r=mesh.gx)
-        #   the derivative apart from a factor that makes sure that the real part of dbf_dx conincides
-        #   with the imaginary part of Q (because the latter is proportional to the derivative of the
-        #   real part of Q).
-        ax.plot(mesh.gx, dQdx_expected[:, 0], 'yo--', label='dbf/dx real')
-        ax.plot(mesh.gx, dQdx_expected[:, 0]*two_pi_K, 'yo', label='dbf/dx real*')
+    # check dQdx against dbf_dx
+    dQdx_expected = dbf_dx(mesh, i=0, r=mesh.gx)
+    #   the derivative apart from a factor that makes sure that the real part of dbf_dx conincides
+    #   with the imaginary part of Q (because the latter is proportional to the derivative of the
+    #   real part of Q).
+    ax.plot(mesh.gx, dQdx_expected[:, 0], 'yo--', label='dbf/dx real')
+    ax.plot(mesh.gx, dQdx_expected[:, 0]*two_pi_K, 'yo', label='dbf/dx real*')
 
-        # ax.plot(mesh.gx, dQdx_expected[:,1], '--', label='dbf/dx imag')
-        for i in range(2*N):
-            print(f"{i=}: real {dQdx[i,0]} {dQdx_expected[i,0]}")
-            assert dQdx[i, 0] == pytest.approx(two_pi_K*dQdx_expected[i, 0])
+    # ax.plot(mesh.gx, dQdx_expected[:,1], '--', label='dbf/dx imag')
+    for i in range(2*N):
+        print(f"{i=}: real {dQdx[i,0]} {dQdx_expected[i,0]}")
+        assert dQdx[i, 0] == pytest.approx(two_pi_K*dQdx_expected[i, 0])
 
-            print(f"{i=}: imag {dQdx[i,1]} {dQdx_expected[i,1]}")
-            assert dQdx[i, 1] == pytest.approx(two_pi_K*dQdx_expected[i, 1])
+        print(f"{i=}: imag {dQdx[i,1]} {dQdx_expected[i,1]}")
+        assert dQdx[i, 1] == pytest.approx(two_pi_K*dQdx_expected[i, 1])
 
-        ratio = dQdx[:, 0] / Q.data[:, 1]
-        print(f"dQdxR/QI={dQdx[:, 0] / Q.data[:, 1]}")
-        assert ratio[0] > 0
-        for rt in ratio:
-            assert rt == pytest.approx(ratio[0])
+    ratio = dQdx[:, 0] / Q.data[:, 1]
+    print(f"dQdxR/QI={dQdx[:, 0] / Q.data[:, 1]}")
+    assert ratio[0] > 0
+    for rt in ratio:
+        assert rt == pytest.approx(ratio[0])
 
-        ratio = dQdx[:, 1] / Q.data[:, 0]
-        print(f"dQdxI/QR={ratio}")
-        assert ratio[0] < 0
-        for rt in ratio:
-            assert rt == pytest.approx(ratio[0])
+    ratio = dQdx[:, 1] / Q.data[:, 0]
+    print(f"dQdxI/QR={ratio}")
+    assert ratio[0] < 0
+    for rt in ratio:
+        assert rt == pytest.approx(ratio[0])
 
-        # ax.plot(mesh.gx, Q_expected[:,0], 'b--')
-        # ax.plot(mesh.gx, Q_expected[:,1], 'r--')
+    # ax.plot(mesh.gx, Q_expected[:,0], 'b--')
+    # ax.plot(mesh.gx, Q_expected[:,1], 'r--')
 
-        # ax.plot(mesh.gx, Q.data[:,0], 'co', label='Q real')
-        # ax.plot(mesh.gx, Q.data[:,1], 'c*', label='Q imag')
-        ax.plot(mesh.gx, dQdx[:,0], 'yx', label='dQ/dx real')
-        # ax.plot(mesh.gx, dQdx[:,1], '*', label='dQ/dx imag')
+    # ax.plot(mesh.gx, Q.data[:,0], 'co', label='Q real')
+    # ax.plot(mesh.gx, Q.data[:,1], 'c*', label='Q imag')
+    ax.plot(mesh.gx, dQdx[:,0], 'yx', label='dQ/dx real')
+    # ax.plot(mesh.gx, dQdx[:,1], '*', label='dQ/dx imag')
 
-        plt.legend()
-        fig.savefig(png_folder/f"1D_basis_function_{0}_differentiation.png")
-        plt.close(fig)
+    plt.legend()
+    fig.savefig(png_folder/f"1D_basis_function_{0}_differentiation.png")
+    plt.close(fig)
 
-        # print(f"Q={Q.data}")
-        # print(f"dQ/dx={dQdx}")
+    # print(f"Q={Q.data}")
+    # print(f"dQ/dx={dQdx}")
 
     print("test_differentiate_1D_x_non_reduced finished")
 
@@ -175,8 +175,8 @@ def test_differentiate_1D_x_reduced(debug=False):
     mesh    = LagrangeMesh(dim=1, M=2*N, d=d, reduced=True , highest_derivative_order=1)
     mesh_nr = LagrangeMesh(dim=1, M=2*N, d=d, reduced=False, highest_derivative_order=1)
 
-    bfr  = mesh   .basis_function(ijk=0, r=mesh   .gx)
-    bfnr = mesh_nr.basis_function(ijk=3, r=mesh_nr.gx)
+    bfr , symm_bfr  = mesh   .basis_function(ijk=0, r=mesh   .gx)
+    bfnr, symm_bfnr = mesh_nr.basis_function(ijk=3, r=mesh_nr.gx)
     for i in range(2*N):
         if i<N:
             print(f"bf{i} real: {bfnr[i,0]}")
@@ -191,8 +191,8 @@ def test_differentiate_1D_x_reduced(debug=False):
             print(f"bf{i} imag: {bfnr[i,1]} =? {bfr[i-N,1]}")
             assert bfnr[i, 1] == pytest.approx(bfr[i-N, 1])
 
-    Q = Observable(mesh   , data=bfr , symmetry=[1,-1])
-    R = Observable(mesh_nr, data=bfnr, symmetry=[1,-1]) # R for "R"eference.
+    Q = Observable(mesh   , data=bfr , symmetry=symm_bfr )
+    R = Observable(mesh_nr, data=bfnr, symmetry=symm_bfnr) # R for "R"eference.
 
     dQdx = Q.differentiate(axes='x')
     dRdx = R.differentiate(axes='x')
@@ -227,8 +227,8 @@ def test_differentiate_1D_xx():
         mesh = LagrangeMesh(dim=1, M=2*N, d=d, reduced=reduced)
 
         if reduced == False:
-
-            Q = Observable(mesh, data = mesh.basis_function(ijk=0, r=mesh.gx))
+            bfq, symm_bfq = mesh.basis_function(ijk=0, r=mesh .gx)
+            Q = Observable(mesh, data=bfq, symmetry=symm_bfq)
             d2Qdx2 = Q.differentiate(axes='xx')
 
             # Re{d2Qdx2} is proportional to -Re{Q}
@@ -266,8 +266,8 @@ def test_differentiate_1D_xxx():
         mesh = LagrangeMesh(dim=1, M=2*N, d=d, reduced=reduced, highest_derivative_order=3)
 
         if reduced == False:
-
-            Q = Observable(mesh, data = mesh.basis_function(ijk=0, r=mesh.gx))
+            bfq, symm_bfq = mesh.basis_function(ijk=0, r=mesh .gx)
+            Q = Observable(mesh, data=bfq, symmetry=symm_bfq)
             d3Qdx3 = Q.differentiate(axes='xxx')
 
             # Re{d3Qdx3} is proportional to Im{Q}
@@ -303,7 +303,8 @@ def test_differentiate_2D_Hessian(debug=False):
         # True,
     ]:
         mesh = LagrangeMesh(dim=dim, M=2*N, d=1., reduced=reduced)
-        Q = Observable(mesh, data=mesh.basis_function(ijk=(0,0), r=mesh.grid))
+        bfq, symm_bfq = mesh.basis_function(ijk=(0,0), r=mesh.grid)
+        Q = Observable(mesh, data=bfq, symmetry=symm_bfq)
         H = Q.differentiate(axes='Hessian',debug=debug)
         print("test_differentiate_2D_Hessian finished")
 
@@ -316,7 +317,8 @@ def test_differentiate_3D_Hessian(debug=False):
         # True,
     ]:
         mesh = LagrangeMesh(dim=dim, M=2*N, d=1., reduced=reduced)
-        Q = Observable(mesh, data=mesh.basis_function(ijk=(0,0,0), r=mesh.grid))
+        bfq, symm_bfq = mesh.basis_function(ijk=(0,0,0), r=mesh.grid)
+        Q = Observable(mesh, data=bfq, symmetry=symm_bfq)
         Q.differentiate(axes='Hessian', debug=debug)
 
         with pytest.raises(ValueError):