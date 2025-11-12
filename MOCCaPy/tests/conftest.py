aec640ee9c2590c5ae6852a8045e5d928e4cd00e
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-11-12 23:33:58 +0100

    Document

--- MOCCaPy/tests/conftest.py
+++ MOCCaPy/tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
-print(f" <<< Executing {__file__} >>>")
-
 
 def pytest_addoption(parser):
+    """Add options to pytest.
+
+    '--no-plot' option allows inhibiting plotting using the `no_plot` fixture (see below).
+    """
     parser.addoption(
         "--no-plot", action="store_true",
         help="Do not produce and save plots (to speed up)"
--- MOCCaPy/tests/test_no_plot.py
+++ MOCCaPy/tests/test_no_plot.py
@@ -1,5 +1,8 @@
 
-from mocca.util import started_finished
+import inspect
+
+from mocca.util import started_finished, started, finished
+
 
 def test_answer(no_plot, debug=False):
     if no_plot:
@@ -7,10 +10,20 @@ def test_answer(no_plot, debug=False):
     else:
         print("\nplotting\n")
 
-# This one FAILS. The decorator causes the fixture noplot not to resolve.
+# This one FAILS. The `started_finished` decorator causes the fixture noplot not to resolve.
 # @started_finished
 # def test_answer_decorated(no_plot, debug=False):
 #     if no_plot:
 #         print("\nnot plotting\n")
 #     else:
 #         print("\nplotting\n")
+
+def test_answer_revisited(no_plot, debug=False):
+    started(inspect.stack()[0][3])
+
+    if no_plot:
+        print("\nnot plotting\n")
+    else:
+        print("\nplotting\n")
+
+    finished(inspect.stack()[0][3])
