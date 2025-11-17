c3b3761a79489fb2a6455719ddfe352a177e8546
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-11-17 21:18:24 +0100

    Remove TODO

--- MOCCaPy/mocca/mean_field/states/slaterdeterminant.py
+++ MOCCaPy/mocca/mean_field/states/slaterdeterminant.py
@@ -6,16 +6,6 @@ class SlaterDeterminant(BCSState):
     BCS mean-field states are characterised by occupancies ($\rho$) being a
     diagonal matrix whose elements are 0 or 1, and thus _isa_ `BCSState`.
     """
-    # TODO: derive from BCSState, which must derive from BogoliubovState. Move data members as needed.
-    #       ?? do we need three different classes? Or do their properties emerge from the way they are
-    #       computed?
-    #       ?? Check issue 'mean-field state classes' #42
-    #       https://realpython.com/inheritance-composition-python/
-    #       Inheritance models an `is a` relationship: a square is also a rectangle, a
-    #       SlaterDeterminant is also a BCSState, and a BCSState  is also BogoliubovState.
-    #       A derived class is said to derive, inherit, or extend a base class
-    #       So far, SlaterDeterminant is very little more than a wrapper for HFPsi...
-
 
     def __init__(self, hfpsi):
         """Mean-field state based on a Slater determinant.
@@ -28,25 +18,33 @@ class SlaterDeterminant(BCSState):
 
     @property
     def n_neutrons(self):
+        """Return the number of neutrons in this mean-field state."""
         return self.hfpsi.n_neutrons
 
     @property
     def n_protons(self):
+        """Return the number of protons in this mean-field state."""
         return self.hfpsi.n_protons
 
     @property
     def n_neutron_wf(self):
+        """Return the number of neutron single particle wave functions in this mean-field state."""
         return self.hfpsi.n_neutron_wf
 
     @property
     def n_proton_wf(self):
+        """Return the number of proton single particle wave functions in this mean-field state."""
+        return self.hfpsi.n_proton_wf
+
+    @property
+    def n_total_wf(self):
+        """Return the total number of single particle wave functions in this mean-field state."""
         return self.hfpsi.n_proton_wf
 
     @property
     def mesh(self):
+        """Return the mesh on which this mean-field state is discretised."""
         return self.hfpsi.mesh
-
-    #
 
     def energy(self, edf=None) -> float:
         """"""
