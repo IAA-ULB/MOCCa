5bede62ff0f5dc61fe12ed93b623515a723dc8d0
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-11-23 22:13:09 +0100

    Update insert_path.py

--- MOCCaPy/scripts/insert_path.py
+++ MOCCaPy/scripts/insert_path.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import sys
 
-def insert_path(rel_path=''):
-    """ Insert path MOCCaPy/rel_path in sys.path."""
+def insert_path(rel_path='', verbose=False):
+    """ Insert path MOCCaPy/rel_path in sys.path.
+    E.g. to be able to import
+      - tests (MOCCaPy/tests): call `insert_path()`
+      - sandbox/*.py (MOCCaPy/sandbox/*.py): call `insert_path('sandbox')`
+    """
     path = Path(__file__).parent
     while path.name != "MOCCaPy":
         path = path.parent
@@ -10,3 +14,5 @@ def insert_path(rel_path=''):
     if not path.exists():
         raise FileNotFoundError(f"Folder '{path}' does not exist.")
     sys.path.insert(0, str(path))
+    if verbose:
+        print(f"inserted path: {path}")
