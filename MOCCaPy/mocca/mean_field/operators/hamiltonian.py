Replace HamiltonianWoodsSaxon with HamiltonianWoodsSaxon2, Fix sign of KineticEnergyOperator

MOCCaPy/mocca/mean_field/operators/hamiltonian.py
@@ -33,6 +33,12 @@ class KineticEnergyOperator(Operator):
                 raise ValueError(f"{extra_derivatives=}, must be a string or a list, not {type(extra_derivatives)}.")
 
         self.nabla = 'xx' if self.mesh.dim == 1 else 'Laplacian'
+        # The kinetic energy term has a minus sign which we incorporate in hbm.
+        if isinstance(hbm, float):
+            if hbm > 0:
+                hbm = -hbm
+        else:
+            hbm = [ -h if h > 0 else h for h in hbm]
         self.hbm = hbm
 
     def apply_base_operator(self):
@@ -41,6 +47,7 @@ class KineticEnergyOperator(Operator):
         self.O_ket.d3 = self.O_ket.data.reshape(self.O_ket.spwf_shape, order='F')
 
         if isinstance(self.hbm, float):
+            assert self.hbm < 0, "hbm must incorporate the minus sign in the Kinetic Energy Operator"
             # Neutrons and protons are treated equally (mass)
             self.O_ket.data[:, :] = self.hbm * self.ket.derivatives[self.nabla]
 
@@ -48,6 +55,7 @@ class KineticEnergyOperator(Operator):
             # Neutrons and protons are treated differently (mass)
             hbm_n = self.hbm[0]
             hbm_p = self.hbm[1]
+            assert (hbm_n < 0) and (hbm_p < 0), "hbm must incorporate the minus sign in the Kinetic Energy Operator"
             # Blocks[0:4] are for neutrons
             # Blocks[4:8] are for protons
             n = self.O_ket.hfblockrange[3][1]  # end of neutron range in the spwfs and begin of proton range
@@ -59,72 +67,72 @@ class KineticEnergyOperator(Operator):
 # ==============================================================================
 # Derived classes
 # ==============================================================================
-class HamiltonianWoodsSaxon(Operator):
-    """Compute the matrix representation of the hamiltonian h with a Woods-Saxon potential. <hfpsi|h|hfpsi>."""
+# class HamiltonianWoodsSaxon(Operator):
+#     """Compute the matrix representation of the hamiltonian h with a Woods-Saxon potential. <hfpsi|h|hfpsi>."""
+#
+#     def __init__(self, hfpsi, hbm=20.73553000, V0=-50.0, r0=1.25, a=0.5):
+#         """Initialize the operator with a wave function `hfpsi` to operate on and set the parameters for the
+#         operator.
+#
+#         Args:
+#             hbm: prefactor of the kinetic energy operator. A single value considers the masses of neutrons
+#                 and protons to be identical, a tuple of 2 values (hbar^2/2m_n, hbar^2/2m_p) considers
+#                 the masses to differ.
+#             V0, r0, a:  parameters of the Woods-Saxon potential
+#                 (cfr https://en.wikipedia.org/wiki/Woods–Saxon_potential).
+#         """
+#         super().__init__(hfpsi)
+#         self.hbm = hbm
+#         self.V0 = V0
+#         self.ainv = 1 / a
+#         self.R = r0 * np.pow(hfpsi.n_neutrons + hfpsi.n_protons, 1 / 3)
+#         self.derivatives = ['Laplacian' if self.mesh.dim >= 2 else 'xx']
+#
+#     def add_local_terms(self):
+#         """Create self.O_ket, fill it with the Woods-Saxon potential, and add the kinetic energy."""
+#         self.O_ket = self.ket.clone()
+#         self.O_ket.d3 = self.O_ket.data.reshape(self.O_ket.spwf_shape, order='F')
+#
+#         # TODO: speed up with numba decorators?
+#
+#         def V_WoodsSaxon1D(r):
+#             return self.V0 / (1. + np.exp(self.ainv * (r - self.R)))
+#
+#         def V_WoodsSaxon2D(x, y):
+#             return self.V0 / (1. + np.exp(self.ainv * (np.sqrt(x * x + y * y) - self.R)))
+#
+#         def V_WoodsSaxon3D(x, y, z):
+#             return self.V0 / (1. + np.exp(self.ainv * (np.sqrt(x * x + y * y + z * z) - self.R)))
+#
+#         dim = self.mesh.dim
+#         if dim == 3:
+#             V_WoodsSaxon = V_WoodsSaxon3D
+#         elif dim == 2:
+#             V_WoodsSaxon = V_WoodsSaxon2D
+#         else:  # dim == 1:
+#             V_WoodsSaxon = V_WoodsSaxon1D
+#
+#         # aply the Woods-Saxon potential
+#         Vr = self.mesh.apply(V_WoodsSaxon)
+#         self.O_ket.data = Vr * self.ket.data
+#
+#         nabla = 'Laplacian' if self.mesh.dim >= 2 else \
+#             'xx'
+#         if isinstance(self.hbm, float):
+#             # Neutrons and protons are treated equally (mass)
+#             self.O_ket.data += self.hbm * self.ket.derivatives[nabla]
+#         else:
+#             # Neutrons and protons are treated differently (mass)
+#             hbm_n = self.hbm[0]
+#             hbm_p = self.hbm[1]
+#             # Blocks[0:4] are for neutrons
+#             # Blocks[4:8] are for protons
+#             n = self.O_ket.hfblockrange[3][1]  # end of neutron range in the spwfs and begin of proton range
+#             self.O_ket.data[:, :, :n] += hbm_n * self.ket.derivatives[nabla][:, :, :n]
+#             self.O_ket.data[:, :, n:] += hbm_p * self.ket.derivatives[nabla][:, :, n:]
 
-    def __init__(self, hfpsi, hbm=20.73553000, V0=-50.0, r0=1.25, a=0.5):
-        """Initialize the operator with a wave function `hfpsi` to operate on and set the parameters for the
-        operator.
 
-        Args:
-            hbm: prefactor of the kinetic energy operator. A single value considers the masses of neutrons
-                and protons to be identical, a tuple of 2 values (hbar^2/2m_n, hbar^2/2m_p) considers
-                the masses to differ.
-            V0, r0, a:  parameters of the Woods-Saxon potential
-                (cfr https://en.wikipedia.org/wiki/Woods–Saxon_potential).
-        """
-        super().__init__(hfpsi)
-        self.hbm = hbm
-        self.V0 = V0
-        self.ainv = 1 / a
-        self.R = r0 * np.pow(hfpsi.n_neutrons + hfpsi.n_protons, 1 / 3)
-        self.derivatives = ['Laplacian' if self.mesh.dim >= 2 else 'xx']
-
-    def add_local_terms(self):
-        """Create self.O_ket, fill it with the Woods-Saxon potential, and add the kinetic energy."""
-        self.O_ket = self.ket.clone()
-        self.O_ket.d3 = self.O_ket.data.reshape(self.O_ket.spwf_shape, order='F')
-
-        # TODO: speed up with numba decorators?
-
-        def V_WoodsSaxon1D(r):
-            return self.V0 / (1. + np.exp(self.ainv * (r - self.R)))
-
-        def V_WoodsSaxon2D(x, y):
-            return self.V0 / (1. + np.exp(self.ainv * (np.sqrt(x * x + y * y) - self.R)))
-
-        def V_WoodsSaxon3D(x, y, z):
-            return self.V0 / (1. + np.exp(self.ainv * (np.sqrt(x * x + y * y + z * z) - self.R)))
-
-        dim = self.mesh.dim
-        if dim == 3:
-            V_WoodsSaxon = V_WoodsSaxon3D
-        elif dim == 2:
-            V_WoodsSaxon = V_WoodsSaxon2D
-        else:  # dim == 1:
-            V_WoodsSaxon = V_WoodsSaxon1D
-
-        # aply the Woods-Saxon potential
-        Vr = self.mesh.apply(V_WoodsSaxon)
-        self.O_ket.data = Vr * self.ket.data
-
-        nabla = 'Laplacian' if self.mesh.dim >= 2 else \
-            'xx'
-        if isinstance(self.hbm, float):
-            # Neutrons and protons are treated equally (mass)
-            self.O_ket.data += self.hbm * self.ket.derivatives[nabla]
-        else:
-            # Neutrons and protons are treated differently (mass)
-            hbm_n = self.hbm[0]
-            hbm_p = self.hbm[1]
-            # Blocks[0:4] are for neutrons
-            # Blocks[4:8] are for protons
-            n = self.O_ket.hfblockrange[3][1]  # end of neutron range in the spwfs and begin of proton range
-            self.O_ket.data[:, :, :n] += hbm_n * self.ket.derivatives[nabla][:, :, :n]
-            self.O_ket.data[:, :, n:] += hbm_p * self.ket.derivatives[nabla][:, :, n:]
-
-
-class HamiltonianWoodsSaxon2(KineticEnergyOperator):
+class HamiltonianWoodsSaxon(KineticEnergyOperator):
     """Compute the matrix representation of the hamiltonian h with a Woods-Saxon potential. <hfpsi|h|hfpsi>.
 
     Does the same as the class above, but derives from KineticEnergyOperator.

MOCCaPy/tests/mocca/mean_field/operators/test_operators.py
@@ -44,6 +44,26 @@ def test_Overlap():
     for block in overlap.matrix:
         assert np.all(block == 24.0)
 
+# def test_HamiltonianWoodsSaxon():
+#
+#     mesh = LagrangeMesh(dim=1, M=6, d=1, reduced=False)
+#     nwn, nwp = 15, 15
+#     osc_freq = (0.2, 0.2, 0.2)
+#     hfpsi = HFPsi(n_neutrons=20, n_protons=20,
+#                   n_proton_wf=nwp, n_neutron_wf=nwn,
+#                   mesh=mesh, init=None,
+#                   )
+#     hfpsi.data[:,:] = 1.
+#     hfpsi.hfblockrange = [(0,8),(8,8),
+#                           (8,15),(15,15),
+#                           (15,23),(23,23),
+#                           (23,30),(30,30)
+#                          ]
+#     hws = hamiltonian.HamiltonianWoodsSaxon(hfpsi)
+#     hws.compute_matrix_representation()
+#     # for block in hws.matrix:
+#     #     assert np.all(block == 24.0)
+
 def test_HamiltonianWoodsSaxon():
 
     mesh = LagrangeMesh(dim=1, M=6, d=1, reduced=False)
@@ -60,26 +80,6 @@ def test_HamiltonianWoodsSaxon():
                           (23,30),(30,30)
                          ]
     hws = hamiltonian.HamiltonianWoodsSaxon(hfpsi)
-    hws.compute_matrix_representation()
-    # for block in hws.matrix:
-    #     assert np.all(block == 24.0)
-
-def test_HamiltonianWoodsSaxon2():
-
-    mesh = LagrangeMesh(dim=1, M=6, d=1, reduced=False)
-    nwn, nwp = 15, 15
-    osc_freq = (0.2, 0.2, 0.2)
-    hfpsi = HFPsi(n_neutrons=20, n_protons=20,
-                  n_proton_wf=nwp, n_neutron_wf=nwn,
-                  mesh=mesh, init=None,
-                  )
-    hfpsi.data[:,:] = 1.
-    hfpsi.hfblockrange = [(0,8),(8,8),
-                          (8,15),(15,15),
-                          (15,23),(23,23),
-                          (23,30),(30,30)
-                         ]
-    hws = hamiltonian.HamiltonianWoodsSaxon2(hfpsi)
     hws.compute_matrix_representation()
     # for block in hws.matrix:
     #     assert np.all(block == 24.0)