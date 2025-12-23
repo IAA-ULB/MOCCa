37c82d9cae5f07186775aa74e28bb697a51a1988
Wouter Ryssens <wryssens@ulb.be>
2025-12-23 21:07:50 +0100

    build_index_combinations now active in ProcessFields

--- src_heph/heph_fields.py
+++ src_heph/heph_fields.py
@@ -26,42 +26,6 @@ from src_heph.heph_densities    import *
 # List of fields needed 
 Fields_needed         = []
 Pairing_Fields_needed = []
-
-#def initfields(so):
-#  """
-#    Go over the needed densities and the functional terms and check whether
-#    we have enough derivatives of the spwfs to calculate the fields.
-#  """ 
-#  for term in src_heph.heph_functional.Functional_terms:
-#      (densities, cpl) = src_heph.heph_functional.ParseDensities(term)
-#      # Count the number of derivatives needed in this term
-#      totalder = 0
-#      totallap = 0
-#      for den in densities:
-#          (der,lap,left,right, coupling, cross) = \
-#                                                 ParseOperators(den,so.timelike)
-#          totalder = totalder + der
-#          totallap = totallap + lap
-#      
-#      # Now see that for all densities in this term, the minimum number
-#      # of derivatives is the total one 
-#      for i in range(len(densities)):
-#          den = densities[i]
-#          (der,lap,left,right,coupling,cross) = ParseOperators(den,so.timelike)
-#          for j in range(len(Densities_needed)):
-#              altden = Densities_needed[j]
-#              (altder, altlap, altleft, altright, altcoupling, altcross)     \
-#                                            = ParseOperators(altden,so.timelike)    
-#              if(altleft == left and altright == right):
-#                  # Set minimum derivatives
-#                  deriv_needed[j].append((totallap, totalder))
-#                  if(len(densities)>2):
-#                    # For trilinear and quadrilinear terms, we will need 
-#                    # more derivatives, as the laplacians can "uncouple"
-#                    # for the calculation of the fields
-#                    deriv_needed[j].append((0, totalder+totallap))
-
-#  src_heph.heph_functional.PruneDeriv_needed()
       
 def GenerateFields(so, oldso, ph_pp_decoupl, fam_active):
   """
@@ -100,6 +64,7 @@ def GenerateFields(so, oldso, ph_pp_decoupl, fam_active):
   global sumindices,tab
   
   import src_heph.fortran_templates.GenerateFields_templates as ts 
+  from src_heph.index_juggling import build_index_combinations 
 
   #---------------------------------------------------------------------------
   # For every unique density encountered, we need to figure out the field
@@ -922,114 +887,11 @@ def GenerateFields(so, oldso, ph_pp_decoupl, fam_active):
       if('P' not in den):
         fieldprecon  = fieldprecon + ts.field_precon_start.substitute(dic)
 
-        # TODO: define a dedicated function for this next particular piece of 
-        #       code. It is highly complex, yet identical to the one in the 
-        #       src_heph/heph_densities module. It should be put in one spot
-        #       and abstracted.
-
-        args = list(itertools.product(range(3), repeat=ndim))
-        # Outer loop over all possible indices
-
-        for arg in args:
-          # We have the uncontracted indices. Now construct the combinations of
-          # indices, including contracted ones, that correspond to this. 
-          uncontracted = []
-          if(len(coupling) + len(cross) == 0):
-              # Nothing to do if no couplings needed
-              uncontracted = [arg]
-          else:
-              #-------------------------------------------------------------------
-              # These are all of the combinations needed for the summation indices
-              cont  = itertools.product(range(3), repeat=len(coupling))
-              # All of the possibilities for the vector products
-              crossind = []
-              for i in range(len(cross)):
-                  crossind = crossind  + (Rot_ind(arg[len(coupling) + i]))    
-
-              # Combine all possibilities
-              if(len(crossind) != 0):
-                  # all the combinations , including scalar and vector products
-                  fullcont = []
-                  for s in cont: 
-                      for r in crossind:            
-                          fullcont.append(s + r)
-              elif (threedim != 0 and len(crossind) == 0):
-                  fullcont = []
-                  for s in cont: 
-                      # Modify the number of possibilities for couplings between vector
-                      # products and scalar products. 
-                      threeopt = itertools.product(range(2), repeat=threedim)
-                      for r in threeopt:            
-                          fullcont.append(s + r)
-              elif (threedim != 0 and len(crossind) != 0):
-                  fullcont = []
-                  for s in cont: 
-                     for r in crossind: 
-                      threeopt = itertools.product(range(2), repeat=threedim)
-                      for t in threeopt:            
-                          fullcont.append(s + r + t)
-              else:
-                  # No vector indices, and no scalar-vector
-                  fullcont = cont
-                
-              #-------------------------------------------------------------------
-              # Note that now fullcont contains all of the terms needed for the
-              # particular argument of the left-hand side. 
-              #
-              #  The ordering of the indices is:
-              #    
-              #  ( mu, nu, ...., xsi , mx, nx, ....,zx  ,  ex, ey, ....., ez )  
-              #   < scalar indices >  < vector indices >  < contracted vectors)
-              #
-              #  corresponding to things of the form
-              #
-              #  coupling, (0,1)        cross (0,1)         coupling (0,1,2)
-              #
-              #  meaning 
-              #
-              #  the value of the   | the values of the  |  whether it is the 
-              #  indices in the     | vector indices     |  first term or the 
-              #  summation          |                    |  second in the vector
-              #                     |                    |  product
-              #-------------------------------------------------------------------
-              for c in fullcont:
-                  p  = ()   
-                  ii = 0
-                  for i in range(LeftOperator.dimension + RightOperator.dimension):
-                      found = False                   
-                      for combination in coupling:
-                          if(i in combination): 
-                              if(len(combination) == 2):
-                                  p = p + (c[coupling.index(combination)],)
-                                  found = True
-                              elif(len(combination) == 3):    
-                                  found = True
-                                  if( i == combination[0] ):
-                                      p = p + (c[coupling.index(combination)],)
-                                  elif( i == combination[1]):
-                                      rot = Rot_ind(c[coupling.index(combination)])
-                                      o = threecoupl.index(combination)
-                                      p = p + (rot[c[-1 -o]][0],)
-                                  elif( i == combination[2]):
-                                      rot = Rot_ind(c[coupling.index(combination)])
-                                      o   = threecoupl.index(combination)
-                                      p = p + (rot[c[-1 -o]][1],)
-                      for combination in cross:
-                          if(i==combination[0]): 
-                                  p = p + (c[cross.index(combination) + len(coupling)],)
-                                  found = True
-                          if(i==combination[1]): 
-                                  p = p + (c[cross.index(combination) + len(coupling) +1 ],)
-                                  found = True    
-                      if(not found): 
-                              p = p + (arg[ii],)
-                              ii = ii +1
-                  uncontracted.append(p)
-
+        args_dict = build_index_combinations(LeftOperator.dimension + RightOperator.dimension, ndim, coupling, cross)
+        for arg in args_dict.keys(): # <---- loop over all possible indices
           IND = ''
           for mu in arg: 
               IND = IND + ',' + str(int(abs(mu)+1)) # Python indexes 0:N-1
-
           dic['IND'] = IND
 
           #--------------------------------------------------------------------
@@ -1056,16 +918,9 @@ def GenerateFields(so, oldso, ph_pp_decoupl, fam_active):
           if(extder != 0):
             fieldprecon  = fieldprecon + ts.field_precon_update.substitute(dic)
 
-            for true_arg in uncontracted: 
-              # We do the whole loop but only use the values for the final set of
-              # indices: all terms in a given contraction should have the same 
-              # behaviour under symmetry.
-
-              # The ugly tuple(np.abs( construction is simply because abs doesn't 
-              # accept tuples as arguments, for whatever reasons.
-              larg = tuple(np.abs(true_arg[:LeftOperator.dimension]))
-              rarg = tuple(np.abs(true_arg[LeftOperator.dimension:]))
-
+            # We only need one particular index; all other terms in the field 
+            #  should behave the same under spatial symmetries.
+            true_arg = args_dict[arg][-1]
             # The ugly tuple(np.abs( construction is simply because abs doesn't 
             # accept tuples as arguments, for whatever reasons.
             larg = tuple(np.abs(true_arg[:LeftOperator.dimension]))
@@ -1241,7 +1096,7 @@ def GenerateAction(field, symmetrize, so):
     """
     
     import src_heph.fortran_templates.GenerateAction_templates as ts
-    
+    from src_heph.index_juggling import build_index_combinations
     #---------------------------------------------------------------------------
     WFNames   = ['psi', 'dpsi', 'ddpsi', 'dddpsi', 'ddddpsi']
     Direction = ["X", 'Y', 'Z']
@@ -1358,44 +1213,18 @@ def GenerateAction(field, symmetrize, so):
     
     # all possible values for the arguments of the left-operator
     largs = list(itertools.product(range(3), repeat=ldim))
-
-#-------------------------------------------------------------------------------
-# These multiplicities are a good idea and often useful, but as implemented is
-# not always correct.
-    #-------------------------------------------------------------------
-    # Go over the uncontracted right-indices and get the independent  
-    # components, and the multiplicities. 
-    #larg_stor = []
-    #larg_new  = []
-    #multiplicities         = []    
-    #for true_larg in largs: 
-    #    l_stor = Storage_Mapping(true_larg[:LeftOperator.derorder])
-    #    mult   = Multiplicity(true_larg[:LeftOperator.derorder])
-    #    l_new  = (l_stor,) + true_larg[LeftOperator.derorder:]
-   # 
-   #     Found = False
-   #     for new in larg_stor:
-   #         if(new == l_new):
-   #             Found = True
-   #     if(not Found):
-   #         larg_stor.append(l_new)
-   #        larg_new.append(true_larg)
-   #         multiplicities.append(mult)
-    
-   # largs = larg_new
-#-------------------------------------------------------------------------------
-    
+   
+    # TODO: replace a lot of this logic with an appropriate call to build_index_combinations; 
+    #        this should simplify everything as the whole object could be treated as one!
+    # 
+    #       ALTHOUGH, to generate efficient code, all indices related to the right operator
+    #                 should be grouped for equal values of the left-operator indices.
+    #                 This will minimize the amount of redundant derivatives calls.
     for true_larg in largs:
         
         # reset temp to 0
         expression = expression + ts.temp_ini
-        
-        # Get the multiplicity correct
-        #m = multiplicities[largs.index(true_larg)]
-        
-        #if( m != 1):
-        #    dic['LMULT'] = str(m) + ' * '
-        #else:
+     
         dic['LMULT'] = ''
             
         if(symmetrize == 1 or symmetrize== -1):
@@ -1406,7 +1235,6 @@ def GenerateAction(field, symmetrize, so):
         rargs    = list(itertools.product(range(3), repeat=rdim-len(cross)))
         dic['RMULT'] = ''
         
-        master_rarg = []
         for rarg in rargs:
 
             rarg_uncontracted = []
