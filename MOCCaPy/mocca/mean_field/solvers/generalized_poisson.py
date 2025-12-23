fc46986470f5d5ce68a3a78ce57bc2a75f66d409
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2025-12-23 10:07:55 +0100

    Improve documentation and interface

--- MOCCaPy/mocca/mean_field/solvers/generalized_poisson.py
+++ MOCCaPy/mocca/mean_field/solvers/generalized_poisson.py
@@ -1,12 +1,13 @@
 """
 Solve the generalized Poisson equation
 
-
 $$
 ( a \Delta + b ) F(r) = f(r)
 $$
 
 using the Finite Difference Method.
+
+The current approach uses a 1D stencil in each dimension, and is NOT ready for stencils using diagonal points.
 """
 
 import numpy as np
@@ -15,55 +16,12 @@ from tabulate import tabulate
 import pytest
 from mocca.mesh import LagrangeMesh, MeshQuantity
 
-
 # TODO: implement generalized poisson
 # TODO: time the methods
-# use out= for matvec ?
-
-class GeneralizedPoissonSolverMF:
-    """Generalized Poisson Solver using a matrix-free Finite Differences approach on an
-    embedded grid. Since it is matrix-free, it must use iterative solvers for solviing
-    the linear system. The embedded grid allows for a simple implementation of the symmetry
-    boundary conditions, as it suffices to copy the current approximation of the solution
-    to the embedding grid across the symmetry boundaries.
-    Most of the actual work is done by the Stencil classes.
-    """
-    def __init__(self, mesh, stencil=3):
-        """
-        Args:
-            mesh (LagrangeMesh): Lagrange mesh providing the grid on which the Generalized
-                Poisson equation will be solved, using Finite Differences.
-            stencil: stencil to be used for discretization. Currently, 3-point or 5-point in
-                each dimension.
-        """
-        self.stencil = L3p(mesh) if (stencil == 3) else \
-                       L5p(mesh)
-
-    def assemble_rhs(self, f, boundary_value, symmetry=None):
-        """Assemble the equations:
-          - apply scaling to rhs in case of uniform grid spacing
-          - applying the Dirichlet boundary conditions to the rhs.
-
-        Args:
-            f (ndarray): right hand side of the generalized Poisson equation. Modified during
-                `assemble` and `solve` methods
-            boundary_value (Callable): function that yields the Dirichlet boundary value when
-                applied to the boundary points of the mesh.
-        """
-        self.stencil.assemble_rhs(f, boundary_value)
-
-    def solve(self, method='cg', **kwargs):
-        """Solve the generalized Poisson equation with Dirichlet boundary conditions `bcs` using
-        an the iterative solvers.
-        Args:
-            method: one of the methods in `self.iterative_linear_solvers`
-        """
-
-        return self.stencil.solve(method=method, **kwargs)
-
+# TODO: use out= for matvec ?
 
 class Stencil:
-    """Base class for system of linear equations resulting from a particular stencil"""
+    """Base class for system of linear equations resulting from a particular 1D-stencil."""
     iterative_linear_solvers = {
         'cg'    : sparse.linalg.cg,
         'cgs'   : sparse.linalg.cgs,
@@ -75,7 +33,9 @@ class Stencil:
     def __init__(self, mesh, boundary_width):
         """
         Args:
-            mesh: LagrangeMesh instance of grid that must be embedded
+            mesh: LagrangeMesh instance on which the solution of the Poisson equation is sought.
+            boundary_width: width of the boundary of the stencil expression. Provided by the
+                derived class
         """
         self.mesh = mesh # this is the embedded mesh, not the embedding mesh
         self.boundary_width = boundary_width
@@ -95,8 +55,12 @@ class Stencil:
         )
 
         # Collect boundary points and symmetry points
+
         def remove_corners(condition, idim):
-            """Remove corners from symmetry points"""
+            """Remove corners from symmetry points.
+            Remark:
+                This should not be called for ND stencils with diagonal points.
+            """
             for jdim in range(self.mesh.dim):
                 if jdim != idim:
                     L, R = bmesh.g1D[jdim][ self.boundary_width-1], \
@@ -146,15 +110,27 @@ class Stencil:
         self.v.set_symmetry(symmetry)
 
     def denominator(self, idim=0):
-        """to be overridden by derived class"""
+        """to be overridden by derived class
+        Returns:
+            the denominator of the stencil expression for the Laplacian
+        """
         raise NotImplementedError()
 
     def Astar_v(self):
-        """to be overridden by derived class"""
+        """To be overridden by derived class.
+        Used by matvec and assemble_rhs.
+
+        Returns:
+            the matrix-vector product of $A^*$ and `self.v`, the linear representation of
+            the embedding grid.
+        """
         raise NotImplementedError()
 
     def matvec(self, u):
-        """to be overridden by derived class"""
+        """to be overridden by derived class
+        Embed the current iteration u in the embedding grid, take symmetries into account, and return
+        the matrix-vector product of the current iteration u and the embedded solution.
+        """
         raise NotImplementedError()
 
     def assemble_rhs(self, f, boundary_value):
@@ -193,31 +169,39 @@ class Stencil:
         self.v_[self.bp_l] = 0.  # required by the iterative solvers when calling matvec
 
     def solve(self, method='cg', **kwargs):
-        """Solve the generalized Poisson equation with Dirichlet boundary conditions `bcs` using
-        an the iterative solvers.
+        """Solve the generalized Poisson equation an iterative solver.
         Args:
             method: one of the methods in `self.iterative_linear_solvers`
+            **kwargs: keyword arguments to be passed to the iterative solver.
         """
-        ils = self.iterative_linear_solvers[method]
-        u = ils(self.LO, self.f, **kwargs)
+        iterative_linear_solver = self.iterative_linear_solvers[method]
+        u = iterative_linear_solver(self.LO, self.f, **kwargs)
         return u
 
 
 class L3p(Stencil):
-    """Classical 3 point stencil for the Laplacian with coefficients[1,-2,1]/h**2"""
+    """Classical 3 point 1D stencil for the Laplacian with coefficients [1,-2,1]/h**2."""
     def __init__(self, mesh):
         """
         Args:
-            v MeshQuantity on embedding mesh
+            mesh: LagrangeMesh instance on which the solution to the Poisson equation is sougth.
         """
         super().__init__(mesh, boundary_width=1)
 
     def denominator(self, idim=0):
+        """The denominator of the stencil expression for the Laplacian."""
         h = self.h_uniform if (self.h_uniform != 0) else self.v.mesh.d[idim]
         h = h**2
         return h
 
     def Astar_v(self):
+        """
+        Returns:
+            the matrix-vector product of $A^*$ and `self.v`, the linear representation of
+            the embedding grid.
+
+        Used by matvec and assemble_rhs.
+        """
         if self.mesh.dim == 3:
             if self.h_uniform:
                 asv  = -6 * self.v.dataG[1:-1, 1:-1, 1:-1] \
@@ -267,8 +251,9 @@ class L3p(Stencil):
                     +   self.v_[2:  ]
 
     def matvec(self, u):
-        """Embed u with boundary region and copy symmetry points for reduced axes. Then apply Astar_v."""
-
+        """Embed the current iteration u in the embedding grid, take symmetries into account, and return
+        the matrix-vector product of the current iteration u and the embedded solution.
+        """
         if self.mesh.dim >= 2:
             if self.mesh.dim == 3:
                 self.v.dataG[1:-1, 1:-1, 1:-1, 0] = u.reshape(self.mesh.N, order='F')
@@ -288,7 +273,7 @@ class L3p(Stencil):
 
 
 class L5p(Stencil):
-    """Classical 5 point stencil for the Laplacian with coefficients[-1, 16, -30, 16, -1]/12*h**2"""
+    """Classical 5 point 1D stencil for the Laplacian with coefficients [-1, 16, -30, 16, -1]/12*h**2."""
     def __init__(self, mesh):
         """
         Args:
@@ -297,11 +282,19 @@ class L5p(Stencil):
         super().__init__(mesh, boundary_width=2)
 
     def denominator(self, idim=0):
+        """The denominator of the stencil expression for the Laplacian."""
         h = self.h_uniform if (self.h_uniform != 0) else self.v.mesh.d[idim]
         h = 12 * h**2
         return h
 
     def Astar_v(self):
+        """
+        Returns:
+            the matrix-vector product of $A^*$ and `self.v`, the linear representation of
+            the embedding grid.
+
+        Used by matvec and assemble_rhs.
+        """
         if self.mesh.dim == 3:
             if self.h_uniform:
                 # np.multiply(-4.0, self.v.dataG[bw  :-bw  , bw  :-bw  ], out=self.uG)
@@ -373,7 +366,9 @@ class L5p(Stencil):
                           +self.v_[ :-4])
 
     def matvec(self, u):
-        """Embed u with boundary region and copy symmetry points for reduced axes. Then apply Astar_v."""
+        """Embed the current iteration u in the embedding grid, take symmetries into account, and return
+        the matrix-vector product of the current iteration u and the embedded solution.
+        """
         if self.mesh.dim == 3:
             self.v.dataG[2:-2, 2:-2, 2:-2, 0] = u.reshape(self.mesh.N, order='F')
             for idim in range(self.mesh.dim):
@@ -395,4 +390,46 @@ class L5p(Stencil):
                 self.v_[1] = self.v.symmetry[0] * self.v_[2]
 
         return self.Astar_v()
+
+
+class GeneralizedPoissonSolverMF:
+    """Generalized Poisson Solver using a matrix-free Finite Differences approach on an
+    embedded grid. Since it is matrix-free, it must use iterative solvers for solving
+    the linear system. The embedded grid allows for a simple implementation of the symmetry
+    boundary conditions, as it suffices to copy the current approximation of the solution
+    to the embedding grid across the symmetry boundaries.
+    Most of the actual work is done by the Stencil classes.
+    """
+    def __init__(self, mesh, stencil=L5p):
+        """
+        Args:
+            mesh (LagrangeMesh): Lagrange mesh providing the grid on which the Generalized
+                Poisson equation will be solved, using Finite Differences.
+            stencil: A Stencil class,  Currently, `L3p` or `L5p`, with resp 3-point and 5-point discretization
+                in each dimension.
+        """
+        self.stencil = stencil(mesh)
+
+    def assemble_rhs(self, f, boundary_value, symmetry=None):
+        """Assemble the equations:
+          - apply scaling to rhs in case of uniform grid spacing
+          - applying the Dirichlet boundary conditions to the rhs.
+
+        Args:
+            f (ndarray): right hand side of the generalized Poisson equation. Modified during
+                `assemble` and `solve` methods
+            boundary_value (Callable): function that yields the Dirichlet boundary value when
+                applied to the boundary points of the mesh.
+        """
+        self.stencil.assemble_rhs(f, boundary_value)
+
+    def solve(self, method='cg', **kwargs):
+        """Solve the generalized Poisson equation with Dirichlet boundary conditions `bcs` using
+        an the iterative solvers.
+        Args:
+            method: one of the methods in `self.iterative_linear_solvers`
+        """
+
+        return self.stencil.solve(method=method, **kwargs)
+
 
