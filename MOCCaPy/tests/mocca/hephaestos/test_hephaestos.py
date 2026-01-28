6459e6d02ec72152fcd09a55689f20f1ed1f0fe8
Wouter Ryssens <wryssens@ulb.be>
2026-01-28 19:56:36 +0100

    Added basic test for NEW hephaestos
     test_hephaestos.py now reads a NLO.func file - and is succesfull
     if it does not raise an exception

--- MOCCaPy/tests/mocca/hephaestos/test_hephaestos.py
+++ MOCCaPy/tests/mocca/hephaestos/test_hephaestos.py
@@ -1,4 +1,23 @@
-from hephaestos import *
+"""
+    PyTest-compatible tests for the hephaestos submodule
+"""
+import glob
+import pytest
+from mocca.hephaestos.hephaestos import read_functional_from_file
 
-def test_read_functional_from_file():
-  dic = read_functional_from_file('func_files/NLO.func')
+func_names = glob.glob('mocca/hephaestos/func_files/*.func')
+
+@pytest.mark.parametrize("func_file", func_names)
+def test_read_functional_from_file(func_file):
+    """
+        Execute the read_functional_from_file function for a given 
+
+        Args:
+            func_file :  string 
+                file containing the functional specification
+    """
+    dic = read_functional_from_file(func_file)
+    print(dic['coupling_constants'])
+
+if __name__ == "__main__":
+    test_read_functional_from_file("NLO.func")
