c0382b81dc0499bfe2e86099f8be8328288babed
Wouter Ryssens <wouter.ryssens@ulb.be>
2026-08-11 14:29:24 +0200

    Updated saving of N.fam and removed precision from fam_pairing_zero_mode

--- testing/integration/fam_pairing_zero_mode.sh
+++ testing/integration/fam_pairing_zero_mode.sh
@@ -177,7 +177,6 @@ operator_type='particle number'
 maxiter=30
 maxhist=31
 eff_charge_n=0.0
-fam_precision=1e-11
 /
 EOF
 
@@ -255,7 +254,7 @@ if $verbose; then
 fi
 
 # Clean up - keeping N.fam!
-cp N.fam ../logs/N.fam
+cp N.fam ../logs/fam_pairing_zero_mode.${args[0]}.N.fam
 teardown_test_env
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
